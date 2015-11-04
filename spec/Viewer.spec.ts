e2b4e49f1c8778b7eceb87ac63bc90561c9d758e
Kamil Nikel <knikel@posteo.de>
2015-11-04 15:17:04 +0100

    Add Jasmine type definitions

--- spec/Viewer.spec.ts
+++ spec/Viewer.spec.ts
@@ -1,3 +1,7 @@
+///<reference path="../typings/jasmine/jasmine.d.ts" />
+
+declare var mapillaryjs: any;
+
 import Viewer from "../src/Viewer"
 var viewer = new mapillaryjs.Viewer('test', {'node': 'testing'})
 
