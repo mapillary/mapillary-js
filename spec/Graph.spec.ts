e563d4d593b8f7f2d1760a62f77774f11474804b
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2015-11-05 18:17:33 -0800

    Add last known Graph test and here is where looping starts failing

--- spec/Graph.spec.ts
+++ spec/Graph.spec.ts
@@ -52,13 +52,44 @@ describe("Graph.insertNodes", () => {
 
         graph.insertNodes(data);
 
-        var node = graph.node(key1)
-        expect(node.key).toEqual(key1)
-        expect(node.sequence.key).toEqual(skey1)
+        var node = graph.node(key1);
+        expect(node.key).toEqual(key1);
+        expect(node.sequence.key).toEqual(skey1);
 
-        node = graph.node(key2)
-        expect(node.key).toEqual(key2)
-        expect(node.sequence.key).toEqual(skey1)
+        node = graph.node(key2);
+        expect(node.key).toEqual(key2);
+        expect(node.sequence.key).toEqual(skey1);
     });
 
+
+    it("should have different sequence keys", () => {
+        var key1 = 'key1';
+        var key2 = 'key2';
+        var skey1 = 'skey1';
+        var skey2 = 'skey2';
+
+        data = {
+            ims: [
+                { key: key1 },
+                { key: key2 }
+            ],
+            ss: [
+                { key: skey1, keys: [key1] },
+                { key: skey2, keys: [key2] }
+            ]
+        };
+
+        graph.insertNodes(data);
+
+        var node = graph.node(key1);
+
+        expect(node.key).toEqual(key1);
+        expect(node.sequence.key).toEqual(skey1);
+
+        node = graph.node(key2);
+        console.log(node);
+
+        expect(node.key).toEqual(key2);
+        expect(node.sequence.key).toEqual(skey2);
+    });
 });
