1656d29146b3e1ae695fad0ac8919680038f35ae
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2015-11-09 18:04:44 -0800

    Experiment with promise chain for prefetcher

--- src/viewer/Prefetcher.ts
+++ src/viewer/Prefetcher.ts
@@ -9,7 +9,7 @@ import {IAPINavIm} from "../api/interfaces/IAPINavIm";
 
 /* Internal Interfaces */
 interface IHash {
-    lastUsed: number;
+    lastUsed: Date;
     data: IAPINavIm;
 }
 
@@ -20,6 +20,7 @@ interface IHashes {
 export class Prefetcher {
     private apiV2: APIv2;
     private cacheMaxSize: number;
+    private hashes: IHashes;
 
     constructor (clientId: string, cacheMaxSize?: number) {
         this.apiV2 = new APIv2(clientId);
@@ -28,14 +29,50 @@ export class Prefetcher {
         if (cacheMaxSize != null) {
             this.cacheMaxSize = cacheMaxSize;
         }
+
+        this.hashes = {};
     }
 
     public loadFromKey(key: string): when.Promise<IAPINavIm> {
-        return this.apiV2.nav.im(key);
+        return this.apiV2.nav.im(key).then((data: IAPINavIm): IAPINavIm => {
+            for (var h in data.hs) {
+                if (data.hs.hasOwnProperty(h)) {
+                    let hkey: string = data.hs[h];
+                    if (!this.hashIsCached(hkey)) {
+                        this.addToCache(hkey, data);
+                    }
+                }
+            }
+
+            return data;
+        });
     }
 
-    public loadFromHash(hash: string): when.Promise<IAPINavIm> {
-        return this.apiV2.nav.h(hash);
+    public loadFromHash(hkey: string): when.Promise<IAPINavIm> {
+        if (this.hashIsCached(hkey)) {
+            let hash: IHash = this.getFromCache(hkey);
+            return when(hash.data);
+        } else {
+            return this.apiV2.nav.h(hkey);
+        }
+    }
+
+    private addToCache(hkey: string, data: IAPINavIm): void {
+        let hash: IHash = {
+            data: data,
+            lastUsed: new Date
+        };
+
+        this.hashes[hkey] = hash;
+    }
+
+    private getFromCache(hkey: string): IHash {
+        let hash: IHash = this.hashes[hkey];
+        return hash;
+    }
+
+    private hashIsCached(hkey: string): boolean {
+        return (hkey in this.hashes);
     }
 
 }
