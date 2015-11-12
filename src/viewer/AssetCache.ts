538cc025406611779b101664b0e93cd11d48f146
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2015-11-11 22:45:40 -0800

    Setup interfaces in AssetCache

--- src/viewer/AssetCache.ts
+++ src/viewer/AssetCache.ts
@@ -1,5 +1,23 @@
 import {Node} from "../Graph";
 
+interface ICacheData {
+    lastUsed: Date;
+    cached: boolean;
+    loaded: boolean;
+    data: any;
+    loadFunc: (source: string, subString: string) => boolean;
+    maxSize: number;
+}
+
+interface ICacheDataMap {
+    [index: string]: ICacheData;
+}
+
+interface IAsset {
+    sized: boolean;
+    cacheData: ICacheDataMap;
+}
+
 export class AssetCache {
     public cache(nodes: Node[]): void {
         console.log("CACHE HERE");
