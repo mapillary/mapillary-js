92eb6b3b542cc97ae2cf61b35dc413251e42a739
Kamil Nikel <knikel@posteo.de>
2015-12-01 10:38:28 +0100

    Make sure Gulpfile.js is linted

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,16 @@
 var gulp = require('gulp')
 
 var browserify = require('browserify')
-var del = require('del');
+var del = require('del')
 var documentation = require('gulp-documentation')
 var fs = require('fs')
-var karmaServer = require('karma').Server;
+var KarmaServer = require('karma').Server
 var source = require('vinyl-source-stream')
 var serve = require('gulp-serve')
 var standard = require('gulp-standard')
 var shell = require('gulp-shell')
 var ts = require('gulp-typescript')
 var rename = require('gulp-rename')
-var tsd = require('tsd')
 var tslint = require('gulp-tslint')
 var argv = require('yargs').argv
 
@@ -42,7 +41,7 @@ gulp.task('browserify', ['typescript'], function () {
     standalone: 'Mapillary'
   })
 
-  bundler.transform('brfs');
+  bundler.transform('brfs')
 
   bundler
     .bundle()
@@ -60,9 +59,9 @@ gulp.task('clean', function () {
 })
 
 gulp.task('documentation', ['browserify'], function () {
-    gulp.src(['./build/Viewer.js', './build/API.js'])
-    .pipe(documentation({format: 'html' }))
-    .pipe(gulp.dest('html-documentation'))
+  gulp.src(['./build/Viewer.js', './build/API.js'])
+  .pipe(documentation({format: 'html'}))
+  .pipe(gulp.dest('html-documentation'))
 })
 
 gulp.task('js-lint', function () {
@@ -76,51 +75,50 @@ gulp.task('js-lint', function () {
 gulp.task('serve', ['browserify'], serve('.'))
 
 gulp.task('test', function (done) {
-    var config
-    if (argv.grep) {
-      config = extendKarmaConfig(__dirname + '/karma.conf.js', {
-        client: {
-          args: ['--grep', argv.grep]
-        },
-        singleRun: true
-      })
-    }
-    else {
-      config = extendKarmaConfig(__dirname + '/karma.conf.js', {
-        singleRun: true
-      })
-    }
+  var config
+  if (argv.grep) {
+    config = extendKarmaConfig(__dirname + '/karma.conf.js', {
+      client: {
+        args: ['--grep', argv.grep]
+      },
+      singleRun: true
+    })
+  } else {
+    config = extendKarmaConfig(__dirname + '/karma.conf.js', {
+      singleRun: true
+    })
+  }
 
-    new karmaServer(config, function (exitCode) {
-      if (exitCode) {
-        console.error(exitCode)
-      }
-    }, done).start()
+  new KarmaServer(config, function (exitCode) {
+    if (exitCode) {
+      console.error(exitCode)
+    }
+  }, done).start()
 })
 
 gulp.task('test-watch', function (done) {
-  new karmaServer({
+  new KarmaServer({
     configFile: __dirname + '/karma.conf.js',
     singleRun: false
-  }, done).start();
+  }, done).start()
 })
 
 gulp.task('ts-lint', ['tsd'], function (cb) {
-    var stream = gulp.src(paths.ts.src)
-      .pipe(tslint())
-      .pipe(tslint.report('verbose'))
-    return stream
+  var stream = gulp.src(paths.ts.src)
+    .pipe(tslint())
+    .pipe(tslint.report('verbose'))
+  return stream
 })
 
 gulp.task('tsd', shell.task('./node_modules/tsd/build/cli.js install'))
 
-gulp.task('typescript', ['ts-lint', 'typescript-src', 'typescript-test'], function (cb) {cb()})
+gulp.task('typescript', ['ts-lint', 'typescript-src', 'typescript-test'], function (cb) { cb() })
 
 gulp.task('typescript-src', function () {
-    var stream = gulp.src(paths.ts.src)
-      .pipe(ts(config.ts))
-      .pipe(gulp.dest(paths.ts.dest))
-    return stream
+  var stream = gulp.src(paths.ts.src)
+    .pipe(ts(config.ts))
+    .pipe(gulp.dest(paths.ts.dest))
+  return stream
 })
 
 gulp.task('typescript-test', function () {
@@ -131,13 +129,13 @@ gulp.task('typescript-test', function () {
 })
 
 gulp.task('watch', [], function () {
-    gulp.watch([paths.ts.src, paths.ts.tests], ['browserify'])
+  gulp.watch([paths.ts.src, paths.ts.tests], ['browserify'])
 })
 
 gulp.task('default', ['serve', 'watch'])
 
 // Helpers
 function extendKarmaConfig (path, conf) {
-    conf.configFile = path
-    return conf
+  conf.configFile = path
+  return conf
 }
