34047354d2db0ee5f28a9074d7db7f57a0a16c20
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2015-12-07 09:40:16 +0100

    Document potential edge interface.

--- src/graph/interfaces/IPotentialEdge.ts
+++ src/graph/interfaces/IPotentialEdge.ts
@@ -1,10 +1,41 @@
 import {IAPINavImIm} from "../../API";
 
+/**
+ * Interface that describes the properties for a node that is the destination of a
+ * potential edge from an origin node.
+ *
+ * @interface IPotentialEdge
+ */
 export interface IPotentialEdge {
+    /**
+     * Distance to the origin node.
+     * @property {number} distance
+     */
     distance: number;
+
+    /**
+     * Change in motion with respect to the viewing direction
+     * of the origin node.
+     * @property {number} motionChange
+     */
     motionChange: number;
+
+    /**
+     * Change in viewing direction with respect to the origin node.
+     * @property {number} directionChange
+     */
     directionChange: number;
+
+    /**
+     * General camera rotation with respect to the origin node.
+     * @property {number} rotation
+     */
     rotation: number;
+
+    /**
+     * APINavImIm properties of destination node.
+     * @property {IAPINavImIm} apiNavImIm
+     */
     apiNavImIm: IAPINavImIm;
 }
 
