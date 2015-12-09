d3fd7b6252eca62905b37d9bb29aec9a9a537a4f
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2015-12-09 15:24:07 +0100

    Turn thresholds in edge calc settings.

--- src/graph/edge/EdgeCalculatorSettings.ts
+++ src/graph/edge/EdgeCalculatorSettings.ts
@@ -5,15 +5,25 @@ export class EdgeCalculatorSettings {
     public maxStepDrift: number;
     public preferredStepDistance: number;
 
+    public turnMaxDistance: number;
+    public turnMaxDirectionChange: number;
+    public turnMaxRigDistance: number;
+    public turnMinRigDirectionChange: number;
+
     constructor() {
         this.maxStepDistance = 20;
         this.maxStepDirectionChange = Math.PI / 6;
         this.maxStepDrift = Math.PI / 6;
         this.preferredStepDistance = 4;
+
+        this.turnMaxDistance = 15;
+        this.turnMaxDirectionChange = 2 * Math.PI / 9;
+        this.turnMaxRigDistance = 0.65;
+        this.turnMinRigDirectionChange = Math.PI / 6;
     }
 
     public get maxDistance(): number {
-        return this.maxStepDistance;
+        return Math.max(this.maxStepDistance, this.turnMaxDistance);
     }
 }
 
