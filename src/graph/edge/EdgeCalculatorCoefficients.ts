11c12a55eaf5d23b47f7b095c3d81e91e222441c
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2015-12-09 16:32:04 +0100

    Turn coefficients.

--- src/graph/edge/EdgeCalculatorCoefficients.ts
+++ src/graph/edge/EdgeCalculatorCoefficients.ts
@@ -6,6 +6,11 @@ export class EdgeCalculatorCoefficients {
     public stepSequencePenalty: number;
     public stepMergeCcPenalty: number;
 
+    public turnDistance: number;
+    public turnMotion: number;
+    public turnSequencePenalty: number;
+    public turnMergeCcPenalty: number;
+
     constructor() {
         this.stepPreferredDistance = 4;
         this.stepMotion = 3;
@@ -13,6 +18,10 @@ export class EdgeCalculatorCoefficients {
         this.stepSequencePenalty = 2;
         this.stepMergeCcPenalty = 6;
 
+        this.turnDistance = 4;
+        this.turnMotion = 2;
+        this.turnSequencePenalty = 1;
+        this.turnMergeCcPenalty = 4;
     }
 }
 
