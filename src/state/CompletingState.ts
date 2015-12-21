987ce621ab200f149c197c4766fdd0eb6802657b
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2015-12-21 13:19:57 +0100

    Slice trajectory to get a new reference in constuctor.

--- src/state/CompletingState.ts
+++ src/state/CompletingState.ts
@@ -2,11 +2,12 @@ import {Node} from "../Graph";
 
 export class CompletingState {
     public trajectory: Node[];
+
     private alpha: number;
-    private animationSpeed: number = 0.25;
+    private animationSpeed: number = 0.025;
 
     constructor (trajectory: Node[]) {
-        this.trajectory = trajectory;
+        this.trajectory = trajectory.slice();
         this.alpha = 0;
         this.currentNode = trajectory.length > 0 ? trajectory[0] : null;
     }
@@ -16,7 +17,6 @@ export class CompletingState {
             [this.trajectory[Math.ceil(this.alpha)]].concat(trajectory) :
             trajectory;
         this.alpha = 0;
-        this.currentNode = trajectory.length > 0 ? trajectory[0] : null;
     }
 
     public appendTrajectory(trajectory: Node[]): void {
@@ -26,7 +26,7 @@ export class CompletingState {
     public update(): void {
         if (this.alpha < this.trajectory.length - 1) {
             this.alpha += this.animationSpeed;
-            this.alpha = Math.max(this.alpha, this.trajectory.length - 1);
+            this.alpha = Math.min(this.alpha, this.trajectory.length - 1);
         }
     }
 
