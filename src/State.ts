cfbdab2d9dce890190ab2d8e95d99afc9c330987
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2015-12-21 12:30:02 +0100

    Remove unused node state.

--- src/State.ts
+++ src/State.ts
@@ -1,6 +1,5 @@
 export {StateContext} from "./state/StateContext"
 
-export {NodeState} from "./state/NodeState"
 export {CompletingState} from "./state/CompletingState"
 
 export * from "./state/interfaces/interfaces"
