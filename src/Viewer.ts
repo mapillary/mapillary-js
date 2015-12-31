1adaa80f8b7f5746469e1c7ea0c75cc5adfdea67
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2015-12-30 17:42:29 -0800

    Remove not used AssetCache

--- src/Viewer.ts
+++ src/Viewer.ts
@@ -1,4 +1,3 @@
-export {AssetCache} from "./viewer/AssetCache";
 export {OptionsParser} from "./viewer/OptionsParser"
 export {Viewer} from "./viewer/Viewer";
 export * from "./viewer/interfaces/interfaces";
