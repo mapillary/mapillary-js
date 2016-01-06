3232d47050d5877bfdee9c2df5c40ded24b7cc6b
Kamil Nikel <knikel@posteo.de>
2016-01-06 11:17:55 +0100

    Letterbox the photo in the viewer

--- src/ui/SimpleUI.ts
+++ src/ui/SimpleUI.ts
@@ -10,21 +10,33 @@ export class SimpleUI implements IUI {
     private canvas: HTMLCanvasElement;
     private disposable: rx.IDisposable;
     private navigator: Navigator;
+    private container: HTMLElement;
 
     constructor(container: HTMLElement, navigator: Navigator) {
         this.navigator = navigator;
 
         this.canvas = document.createElement("canvas");
-        this.canvas.width = 320;
-        this.canvas.height = 240;
+        this.canvas.width = container.clientWidth;
+        this.canvas.height = container.clientHeight;
         container.appendChild(this.canvas);
+        this.container = container;
     }
 
     public activate(): void {
         this.disposable = this.navigator.stateService.currentState.subscribe((currentState: ICurrentState) => {
             if (currentState != null && currentState.currentNode != null) {
                 let ctx: any = this.canvas.getContext("2d");
-                ctx.drawImage(currentState.currentNode.image, 0, 0);
+
+                let cw: number = this.container.clientWidth;
+                let ch: number = this.container.clientHeight;
+
+                ctx.fillStyle = "black"; // todo: This should be customizable by the end user
+                ctx.fillRect(0, 0, cw, ch);
+
+                let w: number = ch / 3 * 4;
+                let offsetLeft: number = (cw - w) / 2;
+
+                ctx.drawImage(currentState.currentNode.image, offsetLeft, 0, w, ch);
             }
         });
     }
