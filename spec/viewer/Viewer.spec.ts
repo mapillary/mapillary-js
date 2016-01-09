dbb95c32494584a38fb5b55b2da1da9b5128da93
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2016-01-08 18:24:05 -0800

    Fix the simplest Viewer test

--- spec/viewer/Viewer.spec.ts
+++ spec/viewer/Viewer.spec.ts
@@ -1,6 +1,8 @@
 /// <reference path="../../typings/jasmine/jasmine.d.ts" />
 /// <reference path="../../typings/when/when.d.ts" />
 
+import * as when from "when";
+
 import {IAPINavIm} from "../../src/API";
 import {EdgeConstants} from "../../src/Edge";
 import {Node} from "../../src/Graph";
@@ -25,23 +27,30 @@ describe("Viewer", () => {
     //     }).toThrowError("The function was not called with correct parameters");
     // });
 
-    // it("should move to a key", (done) => {
-    //     spyOn(viewer.navigator.apiV2.nav, 'im').and.callFake(() => {
-    //         let result: IAPINavIm = {
-    //             hs: ["u3ck26d"],
-    //             ims: [{key: "A", rotation: [0, -Math.PI / 2, 0], merge_version: 1, merge_cc: 1, lat: 0, lon: 0}],
-    //             ss: [{key: "SA", keys: ["A", "B"]}],
-    //         };
-    //         return when(result);
-    //     });
-    //
-    //     viewer.moveToKey("A").then((node: Node) => {
-    //         expect(node.key).toBe("A");
-    //         done();
-    //         // viewer.moveDir(EdgeConstants.Direction.NEXT).first().subscribe((node: Node) => {
-    //         //     expect(node.key).toBe("B");
-    //         //     done();
-    //         // });
-    //     });
-    // });
+    it("should move to a key", (done) => {
+        spyOn(viewer.navigator.apiV2.nav, 'im').and.callFake(() => {
+            let result: IAPINavIm = {
+                hs: ["u3ck26d"],
+                ims: [{key: "A", rotation: [0, -Math.PI / 2, 0], merge_version: 1, merge_cc: 1, lat: 0, lon: 0}],
+                ss: [{key: "SA", keys: ["A", "B"]}],
+            };
+            return when(result);
+        });
+
+        viewer.on("moveend", (node: Node) => {
+            expect(node.key).toBe("A");
+            done();
+        });
+
+        viewer.moveToKey("A");
+
+        //.then((node: Node) => {
+        //    expect(node.key).toBe("A");
+        //    done();
+            // viewer.moveDir(EdgeConstants.Direction.NEXT).first().subscribe((node: Node) => {
+            //     expect(node.key).toBe("B");
+            //     done();
+            // });
+        // });
+    });
 });
