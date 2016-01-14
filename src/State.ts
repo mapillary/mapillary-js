17253083f36c8d44d5155f8cf5dd013ebda5388b
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-01-14 09:27:25 +0100

    Remove old state service.

--- src/State.ts
+++ src/State.ts
@@ -1,4 +1,3 @@
-export {ICurrentState, StateService} from "./state/StateService"
 export {FrameGenerator} from "./state/FrameGenerator";
 export {StateService2} from "./state/StateService2";
 export {StateContext} from "./state/StateContext";
