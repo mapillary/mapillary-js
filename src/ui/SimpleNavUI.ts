240ae197e8488a0c2066c9ef60d65ae6424c6c9c
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-01-15 13:39:22 +0100

    Simplify creation of navigation elements.

--- src/ui/SimpleNavUI.ts
+++ src/ui/SimpleNavUI.ts
@@ -18,17 +18,8 @@ export class SimpleNavUI implements IUI {
     private navigator: Navigator;
 
     private elements: { [direction: number]: INavigationElement } = {};
-    private directions: { [direction: number]: string } = {};
 
     constructor(container: Container, navigator: Navigator) {
-        this.directions[EdgeDirection.STEP_FORWARD] = "Forward";
-        this.directions[EdgeDirection.STEP_BACKWARD] = "Backward";
-        this.directions[EdgeDirection.STEP_LEFT] = "Left";
-        this.directions[EdgeDirection.STEP_RIGHT] = "Right";
-        this.directions[EdgeDirection.TURN_LEFT] = "Turnleft";
-        this.directions[EdgeDirection.TURN_RIGHT] = "Turnright";
-        this.directions[EdgeDirection.TURN_U] = "Turnaround";
-
         let uiContainer: HTMLElement = document.createElement("div");
         uiContainer.className = "SimpleNavUI";
         container.element.appendChild(uiContainer);
@@ -38,20 +29,13 @@ export class SimpleNavUI implements IUI {
     }
 
     public activate(): void {
-        this.elements[EdgeDirection.STEP_FORWARD] =
-            this.createElement(EdgeDirection.STEP_FORWARD);
-        this.elements[EdgeDirection.STEP_BACKWARD] =
-            this.createElement(EdgeDirection.STEP_BACKWARD);
-        this.elements[EdgeDirection.STEP_LEFT] =
-            this.createElement(EdgeDirection.STEP_LEFT);
-        this.elements[EdgeDirection.STEP_RIGHT] =
-            this.createElement(EdgeDirection.STEP_RIGHT);
-        this.elements[EdgeDirection.TURN_LEFT] =
-            this.createElement(EdgeDirection.TURN_LEFT);
-        this.elements[EdgeDirection.TURN_RIGHT] =
-            this.createElement(EdgeDirection.TURN_RIGHT);
-        this.elements[EdgeDirection.TURN_U] =
-            this.createElement(EdgeDirection.TURN_U);
+        this.createElement(EdgeDirection.STEP_FORWARD, "Forward");
+        this.createElement(EdgeDirection.STEP_BACKWARD, "Backward");
+        this.createElement(EdgeDirection.STEP_LEFT, "Left");
+        this.createElement(EdgeDirection.STEP_RIGHT, "Right");
+        this.createElement(EdgeDirection.TURN_LEFT, "Turnleft");
+        this.createElement(EdgeDirection.TURN_RIGHT, "Turnright");
+        this.createElement(EdgeDirection.TURN_U, "Turnaround");
 
         for (let k in this.elements) {
              if (this.elements.hasOwnProperty(k)) {
@@ -96,10 +80,8 @@ export class SimpleNavUI implements IUI {
         this.navigator.moveDir(direction).first().subscribe();
     }
 
-    private createElement(direction: EdgeDirection): INavigationElement {
+    private createElement(direction: EdgeDirection, name: string): void {
         let element: HTMLSpanElement = document.createElement("span");
-
-        let name: string = this.directions[direction];
         element.className = `btn Direction Direction${name} DirectionHidden`;
 
         let move: (direction: EdgeDirection) => void = this.move.bind(this);
@@ -107,7 +89,7 @@ export class SimpleNavUI implements IUI {
         let clickStream: rx.Observable<void> = rx.Observable.fromEvent<void>(element, "click");
         let subscription: rx.IDisposable = clickStream.subscribe(() => { move(direction); });
 
-        return { element: element, subscription: subscription };
+        this.elements[direction] = { element: element, subscription: subscription };
     }
 }
 
