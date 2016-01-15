7c8c13d1073900aae6f4a1a485a994a969e1a039
Kamil Nikel <knikel@posteo.de>
2016-01-15 14:49:13 +0100

    Add initial DOM structure to CoverUI

--- src/ui/CoverUI.ts
+++ src/ui/CoverUI.ts
@@ -4,18 +4,45 @@ import {IUI} from "../UI";
 export class CoverUI implements IUI {
     private container: Container;
     private navigator: Navigator;
+    private coverContainer: HTMLElement;
 
     constructor(container: Container, navigator: Navigator) {
         this.container = container;
         this.navigator = navigator;
+
+        let coverContainer: HTMLElement = document.createElement("div");
+        coverContainer.className = "Cover";
+        this.coverContainer = coverContainer;
+        this.container.element.appendChild(this.coverContainer);
+
+        let button: HTMLElement = this.createButton("mapillary-js");
+        this.coverContainer.appendChild(button);
+
+        console.log(this.coverContainer);
     }
 
     public activate(): void {
+        console.log("CoverUI activated");
         return;
     }
 
     public deactivate(): void {
         return;
+    }
+
+    private activateAllUIs (): void {
+        console.log("All other UIs activated, CoverUI DOM node deleted");
+        this.coverContainer.parentNode.removeChild(this.coverContainer);
+    }
+
+    private createButton (text: string): HTMLElement {
+        let button: HTMLElement = document.createElement("button");
+        button.className = "CoverButton";
+        button.innerText = text;
+        button.addEventListener("click", () => {
+            this.activateAllUIs();
+        });
+        return button;
     }
 }
 
