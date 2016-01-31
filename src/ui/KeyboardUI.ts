4e3d9b446819a6156b393085228375ba978e9e6f
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-01-31 15:19:00 +0100

    Handle step directions in keyboard naivgation.
    Check if step direction exist in node edge array before
    invoking navigator move.

--- src/ui/KeyboardUI.ts
+++ src/ui/KeyboardUI.ts
@@ -1,14 +1,18 @@
 /// <reference path="../../node_modules/rx/ts/rx.all.d.ts" />
+/// <reference path="../../typings/underscore/underscore.d.ts" />
 
 import * as rx from "rx";
+import * as _ from "underscore";
 
-import {EdgeDirection} from "../Edge";
+import {EdgeDirection, IEdge} from "../Edge";
 import {UI} from "../UI";
 import {Container, Navigator} from "../Viewer";
+import {IFrame} from "../State";
+import {Node} from "../Graph";
 
-interface INavigationElement {
-    element: HTMLSpanElement;
-    subscription: rx.IDisposable;
+interface IKeyboardFrame {
+    event: KeyboardEvent;
+    frame: IFrame;
 }
 
 export class KeyboardUI extends UI {
@@ -20,17 +24,59 @@ export class KeyboardUI extends UI {
     }
 
     protected _activate(): void {
-        this._disposable = rx.Observable.fromEvent(document, "keydown").subscribe((event: KeyboardEvent): void => {
-            if (event.keyCode === 40) {
-                this._navigator.moveDir(EdgeDirection.STEP_BACKWARD).subscribe();
-            } else if (event.keyCode === 38) {
-                this._navigator.moveDir(EdgeDirection.STEP_FORWARD).subscribe();
-            }
-        });
+        this._disposable = rx.Observable
+            .fromEvent(document, "keydown")
+            .withLatestFrom(
+                this._navigator.stateService.currentState$,
+                (event: KeyboardEvent, frame: IFrame): IKeyboardFrame => {
+                    return { event: event, frame: frame };
+                })
+            .subscribe((kf: IKeyboardFrame): void => {
+                if (!kf.frame.state.currentNode.pano) {
+                    this.navigatePerspective(kf.event, kf.frame.state.currentNode);
+                }
+            });
     }
 
     protected _deactivate(): void {
         this._disposable.dispose();
+    }
+
+    private navigatePerspective(event: KeyboardEvent, node: Node): void {
+        let direction: EdgeDirection = null;
+
+        switch (event.keyCode) {
+            case 37: // left
+                direction = EdgeDirection.STEP_LEFT;
+                break;
+            case 38: // up
+                direction = EdgeDirection.STEP_FORWARD;
+                break;
+            case 39: // right
+                direction = EdgeDirection.STEP_RIGHT;
+                break;
+            case 40: // down
+                direction = EdgeDirection.STEP_BACKWARD;
+                break;
+            default:
+                break;
+        }
+
+        if (direction == null) {
+            return;
+        }
+
+        let directionExist: boolean = _.any(
+            node.edges,
+            (e: IEdge): boolean => {
+                return e.data.direction === direction;
+            });
+
+        if (!directionExist) {
+            return;
+        }
+
+        this._navigator.moveDir(direction).subscribe();
     }
 }
 
