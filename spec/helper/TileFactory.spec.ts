20ac4b9ddc30a723bf80570fe37422323650ea2a
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-02-01 19:28:31 +0100

    Get lat lon bounds method on tile factory.

--- spec/helper/TileFactory.spec.ts
+++ spec/helper/TileFactory.spec.ts
@@ -4,12 +4,17 @@ import * as THREE from "three";
 
 import {GeoCoords} from "../../src/Geo";
 import {IAPINavIm, IAPINavImIm, IAPINavImS} from "../../src/API";
-import {ILatLonAlt} from "../../src/Graph";
+import {ILatLonAlt, ILatLon} from "../../src/Graph";
 
 export interface ITile {
     x: number;
     y: number;
     nodes: number;
+}
+
+export interface ITileBounds {
+    sw: ILatLon;
+    ne: ILatLon;
 }
 
 export class TileFactory {
@@ -27,45 +32,52 @@ export class TileFactory {
         this._originCoords = { alt: 0, lat: 0, lon: 0 };
     }
 
+    public getBounds(hash: string): ITileBounds {
+        let tile: ITile = this._parseHash(hash);
+
+        let leftX: number = tile.nodes * this._nodeDistance * tile.x;
+        let topY: number = tile.nodes * this._nodeDistance * tile.y;
+        let rightX: number = leftX + tile.nodes * this._nodeDistance;
+        let bottomY: number = topY - tile.nodes * this._nodeDistance;
+
+        let bl: number[] =
+            this._geoCoords.lla_from_topocentric(
+                leftX,
+                bottomY,
+                0,
+                this._originCoords.lat,
+                this._originCoords.lon,
+                this._originCoords.alt);
+
+        let tr: number[] =
+            this._geoCoords.lla_from_topocentric(
+                rightX,
+                topY,
+                0,
+                this._originCoords.lat,
+                this._originCoords.lon,
+                this._originCoords.alt);
+
+        return { sw: { lat: bl[0], lon: bl[1] }, ne: { lat: tr[0], lon: tr[1] } };
+    }
+
     public createHash(tile: ITile): string {
         return tile.y.toString() + ":" + tile.x.toString() + ":" + tile.nodes.toString();
     }
 
     public create(hash: string): IAPINavIm {
-        let coords: number[] = hash
-            .split(":")
-            .map((coord: string): number => {
-                return parseInt(coord);
-            });
+        let tile: ITile = this._parseHash(hash);
 
-        if (coords.length !== 3) {
-            throw Error("Tile format must be on the form col:row:nodes");
-        }
-
-        if (coords[2] < 1) {
-            throw Error("Node number must be a positive integer");
-        }
-
-        let x: number = coords[1];
-        let y: number = coords[0];
-        let nodes: number = coords[2];
-
-        return this._createTile({ x: x, y: y, nodes: nodes });
-    }
-
-    private _createTile(tile: ITile): IAPINavIm {
-        let startX: number = tile.nodes * this._nodeDistance * tile.x;
-        let startY: number = tile.nodes * this._nodeDistance * tile.y;
-        let endX: number = startX + tile.nodes * this._nodeDistance;
-        let endY: number = startY + tile.nodes * this._nodeDistance;
+        let leftX: number = tile.nodes * this._nodeDistance * tile.x;
+        let topY: number = tile.nodes * this._nodeDistance * tile.y;
 
         let ims: IAPINavImIm[] = [];
         let ss: IAPINavImS[] = [];
 
         for (let i: number = 0; i < tile.nodes; i++) {
             for (let j: number = 0; j < tile.nodes; j++) {
-                let x: number = startX + this._nodeDistance / 2 + i * this._nodeDistance;
-                let y: number = startY + this._nodeDistance / 2 + j * this._nodeDistance;
+                let x: number = leftX + this._nodeDistance / 2 + i * this._nodeDistance;
+                let y: number = topY - this._nodeDistance / 2 - j * this._nodeDistance;
 
                 let coords: number[] =
                     this._geoCoords.lla_from_topocentric(
@@ -82,7 +94,7 @@ export class TileFactory {
                     calt: coords[2],
                     cfocal: 1,
                     height: 1,
-                    key: this.createHash(tile) + "_" + j.toString() + ":" + i.toString(),
+                    key: hash + "_" + j.toString() + ":" + i.toString(),
                     lat: coords[0],
                     lon: coords[1],
                     merge_version: 7,
@@ -105,11 +117,33 @@ export class TileFactory {
         }
 
         let result: IAPINavIm = {
-            hs: [this.createHash(tile)],
+            hs: [hash],
             ims: ims,
             ss: ss,
         };
 
         return result;
+    }
+
+    private _parseHash(hash: string): ITile {
+        let coords: number[] = hash
+            .split(":")
+            .map((coord: string): number => {
+                return parseInt(coord);
+            });
+
+        if (coords.length !== 3) {
+            throw Error("Tile format must be on the form col:row:nodes");
+        }
+
+        if (coords[2] < 1) {
+            throw Error("Node number must be a positive integer");
+        }
+
+        let x: number = coords[1];
+        let y: number = coords[0];
+        let nodes: number = coords[2];
+
+        return { x: x, y: y, nodes: nodes };
     }
 }
