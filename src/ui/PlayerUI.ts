d85ff7d2784e3241c77c3f2c9f05bf0918835325
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-02-04 11:18:07 +0100

    Player component appending five nodes ahead.
    Improves behaviour for #49.
    
    Restart subscription on every play to ensure that distinct
    filter is cleared.

--- src/ui/PlayerUI.ts
+++ src/ui/PlayerUI.ts
@@ -8,38 +8,27 @@ import {IFrame} from "../State";
 import {IUIConfiguration, IPlayerUIConfiguration, UIService, UI} from "../UI";
 import {Container, Navigator} from "../Viewer";
 
+interface IFrameConf {
+    conf: IPlayerUIConfiguration;
+    frame: IFrame;
+}
+
 export class PlayerUI extends UI {
     public static uiName: string = "player";
+
     private _disposable: rx.IDisposable;
+
+    private nodesAhead: number = 5;
 
     constructor(name: string, container: Container, navigator: Navigator) {
         super(name, container, navigator);
     }
 
-    public _activate(): void {
-        let lastNode: Node = null;
-
-        this._disposable =
-            this._navigator.stateService.currentState$
-            .combineLatest(this._configuration$, (frame: IFrame, conf: IPlayerUIConfiguration): boolean => {
-                if (conf.playing) {
-                    if (lastNode !== frame.state.currentNode) {
-                        lastNode = frame.state.currentNode;
-                        this._navigator.graphService
-                            .nextNode$(
-                                frame.state.trajectory[frame.state.trajectory.length - 1],
-                                EdgeDirection.NEXT).subscribe((nextNode: Node) => {
-                                    this._navigator.stateService.appendNodes([nextNode]);
-                                    console.log(`next node: ${nextNode.key}`);
-                                });
-                        console.log(frame.state.trajectory);
-                    }
-                }
-                return true;
-            }).subscribe();
+    protected _activate(): void {
+        return;
     }
 
-    public _deactivate(): void {
+    protected _deactivate(): void {
         this._disposable.dispose();
     }
 
@@ -49,10 +38,53 @@ export class PlayerUI extends UI {
 
     public play(): void {
         this.configure({playing: true});
+
+        this._disposable = rx.Observable
+            .combineLatest<IFrame, IPlayerUIConfiguration, IFrameConf>(
+                this._navigator.stateService.currentState$,
+                this._configuration$,
+                (frame: IFrame, conf: IPlayerUIConfiguration): IFrameConf => {
+                    return { conf: conf, frame: frame };
+                })
+            .filter(
+                (fc: IFrameConf): boolean => {
+                    return fc.conf.playing &&
+                        fc.frame.state.trajectory.length - 1 - fc.frame.state.currentIndex < this.nodesAhead;
+                })
+            .selectMany<Node>(
+                (fc: IFrameConf): rx.Observable<Node> => {
+                    return this._navigator.graphService
+                        .nextNode$(
+                            fc.frame.state.trajectory[fc.frame.state.trajectory.length - 1],
+                            EdgeDirection.NEXT)
+                        .first();
+                })
+            .filter(
+                (node: Node): boolean => {
+                    if (node == null) {
+                        this._stop();
+                    }
+
+                    return node != null;
+                })
+            .distinct(
+                (node: Node): string => {
+                    return node.key;
+                })
+            .subscribe(
+                (node: Node): void => {
+                    this._navigator.stateService.appendNodes([node]);
+                });
     }
 
     public stop(): void {
+        this._navigator.stateService.cutNodes();
+    }
+
+    private _stop(): void {
         this.configure({playing: false});
+
+        this._disposable.dispose();
     }
 }
 
