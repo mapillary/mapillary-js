9b31d8325e5cc8833b9eb93b1cee7cc71350ebbc
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-02-04 16:55:58 +0100

    Comment all edge directions and remove unused.

--- src/graph/edge/EdgeDirection.ts
+++ src/graph/edge/EdgeDirection.ts
@@ -1,55 +1,69 @@
-/** Enumeration for directions
- * @enum string
+/**
+ * Enumeration for edge directions
+ * @enum {number}
  * @readonly
+ * @description Directions for edges in node graph describing
+ * sequence, spatial and node type relations between nodes.
  */
 export enum EdgeDirection {
+
     /**
-     * Next photo in the sequence
+     * Next node in the sequence
      */
     NEXT = 0,
 
     /**
-     * Previous photo in the sequence
+     * Previous node in the sequence
      */
     PREV = 3,
 
     /**
-     * Step to the photo on the left
+     * Step to the left keeping viewing direction
      */
     STEP_LEFT,
 
     /**
-     * Step to the photo on the right
+     * Step to the right keeping viewing direction
      */
     STEP_RIGHT,
 
     /**
-     * Step to the photo forward (equivalent of moving forward)
+     * Step forward keeping viewing direction
      */
     STEP_FORWARD,
 
     /**
-     * Step to the photo backward (equivalent of moving backward)
+     * Step backward keeping viewing direction
      */
     STEP_BACKWARD,
 
     /**
-     * Rotate ~90deg to the left from the current photo
+     * Turn 90 degrees counter clockwise
      */
     TURN_LEFT,
 
     /**
-     * Rotate ~90deg to the right from the current photo
+     * Turn 90 degrees clockwise
      */
     TURN_RIGHT,
 
     /**
-     * Turn around, relative to the dirrection of the current photo
+     * Turn 180 degrees
      */
     TURN_U,
+
+    /**
+     * Rotate with small counter clockwise angle change
+     */
     ROTATE_LEFT,
+
+    /**
+     * Rotate with small clockwise angle change
+     */
     ROTATE_RIGHT,
-    HOMOGRAPHY,
-    CLOSE,
+
+    /**
+     * Panorama in general direction
+     */
     PANO
 };
