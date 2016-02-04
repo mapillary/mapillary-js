af55d91ed05c167c7e5dbb56fb6ecaacd2c39f8d
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-02-04 16:59:55 +0100

    Docs for image size enum.

--- src/viewer/ImageSize.ts
+++ src/viewer/ImageSize.ts
@@ -1,6 +1,28 @@
+/**
+ * Enumeration for image sizes
+ * @enum {number}
+ * @readonly
+ * @description Image sizes in pixels for the long side of the image.
+ */
 export enum ImageSize {
+
+    /**
+     * 320 pixels image size
+     */
     SIZE_320 = 320,
+
+    /**
+     * 640 pixels image size
+     */
     SIZE_640 = 640,
+
+    /**
+     * 1024 pixels image size
+     */
     SIZE_1024 = 1024,
+
+    /**
+     * 2048 pixels image size
+     */
     SIZE_2048 = 2048,
 }
