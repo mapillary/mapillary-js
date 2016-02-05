a0dd4364ee0e8fd4ee21b7770abbe1e0d1c92eed
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-02-05 15:31:00 +0100

    Distinct before next node to ensure a single call.
    Ensure that async call to next node is only performed
    once by doing distinct on last node in
    trajectory before call. Use state properties instead
    of calculating last and nodes ahead.

--- src/component/PlayerComponent.ts
+++ src/component/PlayerComponent.ts
@@ -112,33 +112,35 @@ export class PlayerComponent extends Component {
                 })
             .filter(
                 (frame: IFrame): boolean => {
-                    let nodesAhead: number = frame.state.trajectory.length - 1 - frame.state.currentIndex;
-
-                    return nodesAhead < this.nodesAhead;
+                    return frame.state.nodesAhead < this.nodesAhead;
+                })
+            .distinctUntilChanged(
+                 (frame: IFrame): string => {
+                     return frame.state.lastNode.key;
+                 })
+            .map<Node>(
+                (frame: IFrame): Node => {
+                    return frame.state.lastNode;
                 })
             .selectMany<Node>(
-                (frame: IFrame): rx.Observable<Node> => {
-                    let originalLast: Node = frame.state.trajectory[frame.state.trajectory.length - 1];
-
+                (node: Node): rx.Observable<Node> => {
                     return this._navigator.graphService
                         .nextNode$(
-                            originalLast,
+                            node,
                             EdgeDirection.NEXT)
                         .withLatestFrom<IFrame, INodes>(
                             this._navigator.stateService.currentState$,
-                            (next: Node, nextFrame: IFrame): INodes => {
-                                let last: Node = nextFrame.state.trajectory[frame.state.trajectory.length - 1];
-
-                                return  { last: last, next: next };
+                            (next: Node, frame: IFrame): INodes => {
+                                 return  { last: frame.state.lastNode, next: next };
                             })
                         .first()
                         .filter(
                             (nodes: INodes): boolean => {
-                                return nodes.last.key === originalLast.key;
+                                return nodes.last.key === node.key;
                             })
                         .map<Node>(
                             (nodes: INodes): Node => {
-                                return nodes.next;
+                               return nodes.next;
                             });
                 })
             .filter(
@@ -148,10 +150,6 @@ export class PlayerComponent extends Component {
                     }
 
                     return node != null;
-                })
-            .distinctUntilChanged(
-                (node: Node): string => {
-                    return node.key;
                 })
             .subscribe(
                 (node: Node): void => {
