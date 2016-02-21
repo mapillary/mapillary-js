70fe693ff343458fc2327e12b39144a9c73ab28f
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-02-21 17:48:37 +0100

    Test that WebGLRenderer is created on render request.

--- spec/render/GLRenderer.spec.ts
+++ spec/render/GLRenderer.spec.ts
@@ -5,14 +5,30 @@
 import * as rx from "rx";
 import * as THREE from "three";
 
-import {GLRenderer} from "../../src/Render";
+import {
+    GLRenderer,
+    GLRenderStage,
+    IGLRender,
+    IGLRenderFunction,
+    IGLRenderHash,
+} from "../../src/Render";
 import {IFrame} from "../../src/State";
+
+class RendererMock implements THREE.Renderer {
+    public domElement: HTMLCanvasElement = document.createElement("canvas");
+
+    public render(s: THREE.Scene, c: THREE.Camera): void { }
+    public setSize(w: number, h: number, updateStyle?: boolean): void { }
+    public setClearColor(c: THREE.Color, o: number): void { }
+    public clear(): void { }
+    public clearDepth(): void { }
+}
 
 describe("GLRenderer.ctor", () => {
     it("should be contructed", () => {
         let element: HTMLDivElement = document.createElement("div");
 
-        let glRenderer: GLRenderer = new GLRenderer(element, rx.Observable.empty<IFrame>())
+        let glRenderer: GLRenderer = new GLRenderer(element, rx.Observable.empty<IFrame>());
 
         expect(glRenderer).not.toBeNull();
     });
@@ -22,8 +38,36 @@ describe("GLRenderer.ctor", () => {
 
         let element: HTMLDivElement = document.createElement("div");
 
-        let glRenderer: GLRenderer = new GLRenderer(element, rx.Observable.empty<IFrame>())
+        let glRenderer: GLRenderer = new GLRenderer(element, rx.Observable.empty<IFrame>());
 
         expect(THREE.WebGLRenderer).not.toHaveBeenCalled();
+    });
+});
+
+describe("GLRenderer.renderer", () => {
+    it("should be created on first render", () => {
+        let rendererMock: RendererMock = new RendererMock();
+
+        spyOn(THREE, "WebGLRenderer").and.returnValue(rendererMock);
+
+        let element: HTMLDivElement = document.createElement("div");
+
+        let glRenderer: GLRenderer = new GLRenderer(element, rx.Observable.empty<IFrame>());
+
+        let renderFunction: IGLRenderFunction = (pc: THREE.PerspectiveCamera, r: THREE.WebGLRenderer): void => { };
+        let render: IGLRender = {
+            frameId: 0,
+            needsRender: false,
+            render: renderFunction,
+            stage: GLRenderStage.Background,
+        }
+        let renderHash: IGLRenderHash = {
+            name: "mock",
+            render: render,
+        }
+
+        glRenderer.render$.onNext(renderHash);
+
+        expect(THREE.WebGLRenderer).toHaveBeenCalled();
     });
 });
