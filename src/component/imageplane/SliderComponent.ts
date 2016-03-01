28d885a63fcbe1747f4769771ef628e82e75803d
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-03-01 18:04:21 +0100

    Show image planes in slider component.
    Render image planes with opacity of one always.

--- src/component/imageplane/SliderComponent.ts
+++ src/component/imageplane/SliderComponent.ts
@@ -1,16 +1,92 @@
-import {Component, ComponentService} from "../../Component";
+/// <reference path="../../../typings/browser.d.ts" />
+
+import * as THREE from "three";
+import * as rx from "rx";
+
+import {ICurrentState, IFrame} from "../../State";
 import {Container, Navigator} from "../../Viewer";
+import {IGLRenderHash, GLRenderStage, IGLRenderFunction} from "../../Render";
+import {Component, ComponentService, ImagePlaneScene, ImagePlaneFactory} from "../../Component";
 
 export class SliderComponent extends Component {
     public static componentName: string = "slider";
+
+    private _subscription: rx.IDisposable;
+
+    private imagePlaneFactory: ImagePlaneFactory;
+    private imagePlaneScene: ImagePlaneScene;
+
+    private currentKey: string;
+    private previousKey: string;
 
     constructor (name: string, container: Container, navigator: Navigator) {
         super(name, container, navigator);
     }
 
-    protected _activate(): void { return; }
+    protected _activate(): void {
+        this.imagePlaneFactory = new ImagePlaneFactory();
+        this.imagePlaneScene = new ImagePlaneScene();
 
-    protected _deactivate(): void { return; }
+        this.currentKey = null;
+        this.previousKey = null;
+
+        let render: IGLRenderFunction = this.render.bind(this);
+
+        this._subscription = this._navigator.stateService.currentState$
+            .map<IGLRenderHash>(
+                (frame: IFrame): IGLRenderHash => {
+                    let needsRender: boolean = this.updateImagePlanes(frame.state);
+
+                    return {
+                        name: this._name,
+                        render: {
+                            frameId: frame.id,
+                            needsRender: needsRender,
+                            render: render,
+                            stage: GLRenderStage.Background,
+                        },
+                    };
+                })
+            .subscribe(this._container.glRenderer.render$);
+    }
+
+    protected _deactivate(): void {
+        this.imagePlaneScene.clear();
+        this._subscription.dispose();
+    }
+
+    private updateImagePlanes(state: ICurrentState): boolean {
+        if (state.currentNode == null) {
+            return false;
+        }
+
+        let needsRender: boolean = false;
+
+        if (state.previousNode != null && this.previousKey !== state.previousNode.key) {
+            needsRender = true;
+            this.previousKey = state.previousNode.key;
+            this.imagePlaneScene.updateImagePlanes([
+                this.imagePlaneFactory.createMesh(state.previousNode, state.previousTransform),
+            ]);
+        }
+
+        if (this.currentKey !== state.currentNode.key) {
+            needsRender = true;
+            this.currentKey = state.currentNode.key;
+            this.imagePlaneScene.updateImagePlanes([
+                this.imagePlaneFactory.createMesh(state.currentNode, state.currentTransform),
+            ]);
+        }
+
+        return needsRender;
+    }
+
+    private render(
+        perspectiveCamera: THREE.PerspectiveCamera,
+        renderer: THREE.WebGLRenderer): void {
+        renderer.render(this.imagePlaneScene.sceneOld, perspectiveCamera);
+        renderer.render(this.imagePlaneScene.scene, perspectiveCamera);
+    }
 }
 
 ComponentService.register(SliderComponent);
