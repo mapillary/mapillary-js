4c308dfff793b0bd8812d9f2b7c358c8586b7ad9
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2016-03-02 07:05:42 -0800

    Add missing typings

---
+++ typings/created/falcor-http-datasource/falcor-http-datasource.d.ts
@@ -0,0 +1,13 @@
+declare class HttpDataSource {
+    constructor(...args: any[]);
+    get(pathSet: any): any;
+    set(jsongEnv: any): any;
+    call(callPath: any, args?: any, pathSuffix?: any, paths?: any): any;
+}
+
+declare module HttpDataSource {
+}
+
+declare module "falcor-http-datasource" {
+    export = HttpDataSource;
+}
---
+++ typings/created/falcor/falcor.d.ts
@@ -0,0 +1,14 @@
+declare module "falcor" {
+    interface IModel {
+        get(pathSet: any): any;
+        set(jsongEnv: any): any;
+        call(callPath: any, args?: any, pathSuffix?: any, paths?: any): any;
+    }
+    export class Model implements IModel {
+        constructor(...args: any[]);
+        get(pathSet: any): any;
+        set(jsongEnv: any): any;
+        call(callPath: any, args?: any, pathSuffix?: any, paths?: any): any;
+    }
+    export default Model;
+}
