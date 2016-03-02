c7a000c7bfaded481b81ed896a7a8d6ecb5105b7
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2016-03-01 17:12:39 -0800

    Update crippled GraphService test

--- spec/graph/GraphService.spec.ts
+++ spec/graph/GraphService.spec.ts
@@ -2,16 +2,18 @@
 
 import * as when from "when";
 
-import {IAPINavIm, APIv2} from "../../src/API";
+import {IAPINavIm, APIv2, APIv3} from "../../src/API";
 import {GraphService, Graph, Node} from "../../src/Graph";
 
 describe("GraphService", () => {
     var apiV2: APIv2;
+    var apiV3: APIv3;
     var graphService: GraphService;
 
     beforeEach(() => {
         apiV2 = new APIv2("MkJKbDA0bnZuZlcxeTJHTmFqN3g1dzo5NWEzOTg3OWUxZDI3MjM4");
-        graphService = new GraphService(apiV2);
+        apiV3 = new APIv3("MkJKbDA0bnZuZlcxeTJHTmFqN3g1dzo5NWEzOTg3OWUxZDI3MjM4");
+        graphService = new GraphService(apiV2, apiV3);
 
 //        spyOn(graphService.tilesService.apiV2.nav, 'im').and.callFake(() => {
 //            let result: IAPINavIm = {
