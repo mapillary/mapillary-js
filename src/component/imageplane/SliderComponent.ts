9e14eea85b324d185fbeb4dd51839603694cc029
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-03-02 08:32:39 +0100

    Set nodes API on slider component.
    API for setting nodes from keys on slider component.
    Use set image planes methods on scene to always
    overwrite old planes.

--- src/component/imageplane/SliderComponent.ts
+++ src/component/imageplane/SliderComponent.ts
@@ -3,10 +3,26 @@
 import * as THREE from "three";
 import * as rx from "rx";
 
+import {Node} from "../../Graph";
 import {ICurrentState, IFrame} from "../../State";
 import {Container, Navigator} from "../../Viewer";
 import {IGLRenderHash, GLRenderStage, IGLRenderFunction} from "../../Render";
 import {Component, ComponentService, ImagePlaneScene, ImagePlaneFactory} from "../../Component";
+
+interface ISliderKeys {
+    background: string;
+    foreground: string;
+}
+
+interface ISliderNodes {
+    background: Node;
+    foreground: Node;
+}
+
+interface ISliderCombination {
+    nodes: ISliderNodes;
+    state: ICurrentState;
+}
 
 export class SliderComponent extends Component {
     public static componentName: string = "slider";
@@ -21,6 +37,51 @@ export class SliderComponent extends Component {
 
     constructor (name: string, container: Container, navigator: Navigator) {
         super(name, container, navigator);
+    }
+
+    public setNodes(sliderKeys: ISliderKeys): void {
+        if (!this._activated) {
+            return;
+        }
+
+        rx.Observable
+            .zip<Node, Node, ISliderNodes>(
+                this._navigator.graphService.node$(sliderKeys.background),
+                this._navigator.graphService.node$(sliderKeys.foreground),
+                (background: Node, foreground: Node): ISliderNodes => {
+                    return { background: background, foreground: foreground };
+                })
+            .withLatestFrom(
+                this._navigator.stateService.currentState$,
+                (nodes: ISliderNodes, frame: IFrame): ISliderCombination => {
+                    return { nodes: nodes, state: frame.state };
+                })
+            .subscribe(
+                (co: ISliderCombination): void => {
+                    if (co.state.currentNode != null &&
+                        co.state.previousNode != null &&
+                        co.state.currentNode.key === co.nodes.foreground.key &&
+                        co.state.previousNode.key === co.nodes.background.key) {
+                        return;
+                    }
+
+                    if (co.state.currentNode.key === co.nodes.background.key) {
+                        this._navigator.stateService.setNodes([co.nodes.foreground]);
+                        return;
+                    }
+
+                    if (co.state.currentNode.key === co.nodes.foreground.key &&
+                        co.state.trajectory.length === 1) {
+                        this._navigator.stateService.prependNodes([co.nodes.background]);
+                        return;
+                    }
+
+                    this._navigator.stateService.setNodes([co.nodes.background]);
+                    this._navigator.stateService.setNodes([co.nodes.foreground]);
+                },
+                (e: Error): void => {
+                    console.log(e);
+                });
     }
 
     protected _activate(): void {
@@ -65,7 +126,7 @@ export class SliderComponent extends Component {
         if (state.previousNode != null && this.previousKey !== state.previousNode.key) {
             needsRender = true;
             this.previousKey = state.previousNode.key;
-            this.imagePlaneScene.updateImagePlanes([
+            this.imagePlaneScene.setImagePlanesOld([
                 this.imagePlaneFactory.createMesh(state.previousNode, state.previousTransform),
             ]);
         }
@@ -73,7 +134,7 @@ export class SliderComponent extends Component {
         if (this.currentKey !== state.currentNode.key) {
             needsRender = true;
             this.currentKey = state.currentNode.key;
-            this.imagePlaneScene.updateImagePlanes([
+            this.imagePlaneScene.setImagePlanes([
                 this.imagePlaneFactory.createMesh(state.currentNode, state.currentTransform),
             ]);
         }
