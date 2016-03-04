270dbf3a95c6ff61ba92c475c84bfaab9b4003b8
Johan Gyllenspetz <johan.gyllenspetz@gmail.com>
2016-03-03 20:39:17 -0800

    Make DetectionComponent use falcor to get real ors

--- typings/created/falcor/falcor.d.ts
+++ typings/created/falcor/falcor.d.ts
@@ -8,6 +8,7 @@ declare module "falcor" {
         constructor(...args: any[]);
         get(pathSet: any): any;
         set(jsongEnv: any): any;
+        treatErrorsAsValues(): any;
         call(callPath: any, args?: any, pathSuffix?: any, paths?: any): any;
     }
     export default Model;
