d28e29ec1da63cd9d5c57a666b3559da5c733407
Kamil Nikel <knikel@posteo.de>
2016-03-05 16:11:39 +0100

    Prevent CircleCI from running for gh-pages commits

--- scripts/push-docs.sh
+++ scripts/push-docs.sh
@@ -5,6 +5,6 @@ git checkout -- .
 git checkout gh-pages
 \cp -rf docs-out/index.html index.html
 git add index.html
-git commit -m "Update docs"
+git commit -m "Update docs [ci skip]"
 git push origin gh-pages
 git checkout master
