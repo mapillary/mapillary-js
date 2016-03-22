80b714a34c7bcc90557c2effc268a20dfd0a916d
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-03-22 10:38:56 +0100

    Make RBush typings non ambient.

--- typings/created/rbush/rbush.d.ts
+++ typings/created/rbush/rbush.d.ts
@@ -1,17 +1,15 @@
-declare module "rbush" {
-    module rbush {
-        interface RBush<T> {
-            insert(item: T): void;
-            remove(item: T): void;
-            search(bbox: [number, number, number, number]): T[];
-            all(): T[];
-            clear(): void;
-        }
+declare module rbush {
+    interface RBush<T> {
+        insert(item: T): void;
+        remove(item: T): void;
+        search(bbox: [number, number, number, number]): T[];
+        all(): T[];
+        clear(): void;
     }
-
-    function rbush<T>(
-        maxEntries: number,
-        accessorStrings?: [string, string, string, string]): rbush.RBush<T>;
-
-    export = rbush;
 }
+
+declare function rbush<T>(
+    maxEntries: number,
+    accessorStrings?: [string, string, string, string]): rbush.RBush<T>;
+
+export = rbush;
