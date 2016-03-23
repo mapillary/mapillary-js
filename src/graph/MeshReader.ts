eaa0201134a8d2dc80d145d03fc796c08fd55569
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-03-23 08:49:07 +0100

    Add Pbf typings and make class generic.
    Use generic Pbf class in mesh reader.

--- src/graph/MeshReader.ts
+++ src/graph/MeshReader.ts
@@ -2,28 +2,35 @@
 
 import {IMesh} from "../Graph";
 
-import * as pbf from "pbf";
+import * as Pbf from "pbf";
+
+interface IFlatMesh {
+    triangles: number[];
+    vertices: number[];
+}
 
 export class MeshReader {
     public static read(buffer: Buffer): IMesh {
-        let pbfMesh: any = new pbf(buffer);
-        return MeshReader.readMesh(pbfMesh);
+        let pbf: Pbf<IFlatMesh> = new Pbf<IFlatMesh>(buffer);
+        return MeshReader.readMesh(pbf);
     }
 
-    private static readMesh(pbf: any): IMesh {
-        let flatMesh: any = pbf.readFields(MeshReader.readMeshField, {"vertices": [], "triangles": []});
+    private static readMesh(pbf: Pbf<IFlatMesh>): IMesh {
+        let flatMesh: IFlatMesh =
+            pbf.readFields(MeshReader.readMeshField, { triangles: [], vertices: [] });
+
         return MeshReader.flatMeshToMesh(flatMesh);
     }
 
-    private static readMeshField(tag: any, mesh: any, pbf: any): any {
+    private static readMeshField(tag: number, flatMesh: IFlatMesh, pbf: Pbf<IFlatMesh>): void {
         if (tag === 1) {
-            mesh.vertices.push(pbf.readFloat());
+            flatMesh.vertices.push(pbf.readFloat());
         } else if (tag === 2) {
-            mesh.triangles.push(pbf.readVarint());
+            flatMesh.triangles.push(pbf.readVarint());
         }
     }
 
-    private static flatMeshToMesh(flatMesh: any): IMesh {
+    private static flatMeshToMesh(flatMesh: IFlatMesh): IMesh {
         let mesh: IMesh = { faces: [], populated: false, vertices: [] };
         let numVertices: number = flatMesh.vertices.length / 3;
         for (let i: number = 0; i < numVertices; ++i) {
--- typings/created/pbf/pbf.d.ts
+++ typings/created/pbf/pbf.d.ts
@@ -1,5 +1,12 @@
-declare class Pbf {
+declare class Pbf<T> {
     constructor(buffer: Buffer);
+
+    readFields(readData: (tag: number, data: T, pbf: Pbf<T>) => void, data: T): T;
+
+    readVarint(): number;
+    readFloat(): number;
+
+    destroy(): void;
 }
 
 declare module Pbf { }
