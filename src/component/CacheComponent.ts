4187182d49dd55be81ec9aa61a055326c8d71fa1
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-03-30 19:06:32 +0200

    Make cache component recursive.
    Enable traversing many steps in a single direction
    by making recursive calls through expand. Cache
    all directions with 1 step depth.
    Do not cache step directions for pano nodes.

--- src/component/CacheComponent.ts
+++ src/component/CacheComponent.ts
@@ -10,68 +10,69 @@ import {Container, Navigator} from "../Viewer";
 export class CacheComponent extends Component {
     public static componentName: string = "cache";
 
-    private _prespectiveDirections: EdgeDirection[];
-    private _panoDirections: EdgeDirection[];
-
     private _nodeSubscription: rx.IDisposable;
 
     constructor(name: string, container: Container, navigator: Navigator) {
         super(name, container, navigator);
-
-        this._prespectiveDirections = [
-            EdgeDirection.Prev,
-            EdgeDirection.Next,
-            EdgeDirection.StepLeft,
-            EdgeDirection.StepRight,
-            EdgeDirection.StepForward,
-            EdgeDirection.StepBackward,
-            EdgeDirection.Pano,
-        ];
-
-        this._panoDirections = [
-            EdgeDirection.Prev,
-            EdgeDirection.Next,
-            EdgeDirection.Pano,
-        ];
     }
 
     protected _activate(): void {
         this._nodeSubscription = this._navigator.stateService.currentNode$
-            .map<string[]>(
-                (node: Node): string[] => {
-                    let keys: string[] = [];
-
-                    for (let edge of node.edges) {
-                        if (node.pano) {
-                            if (this._panoDirections.indexOf(edge.data.direction) === -1) {
-                                continue;
-                            }
-                        } else {
-                            if (this._prespectiveDirections.indexOf(edge.data.direction) === -1) {
-                                continue;
-                            }
-                        }
-
-                        keys.push(edge.to);
-                    }
-
-                    return keys;
-                })
             .flatMapLatest<Node>(
-                (keys: string[]): rx.Observable<Node> => {
+                (node: Node): rx.Observable<Node> => {
+                    let next$: rx.Observable<Node> = this._cache$(node, EdgeDirection.Next, 1);
+                    let prev$: rx.Observable<Node> = this._cache$(node, EdgeDirection.Prev, 1);
+                    let pano$: rx.Observable<Node> = this._cache$(node, EdgeDirection.Pano, 1);
+
+                    let forward$: rx.Observable<Node> = node.pano ?
+                        rx.Observable.empty<Node>() :
+                        this._cache$(node, EdgeDirection.StepForward, 1);
+
+                    let backward$: rx.Observable<Node> = node.pano ?
+                        rx.Observable.empty<Node>() :
+                        this._cache$(node, EdgeDirection.StepBackward, 1);
+
+                    let left$: rx.Observable<Node> = node.pano ?
+                        rx.Observable.empty<Node>() :
+                        this._cache$(node, EdgeDirection.StepLeft, 1);
+
+                    let right$: rx.Observable<Node> = node.pano ?
+                        rx.Observable.empty<Node>() :
+                        this._cache$(node, EdgeDirection.StepRight, 1);
+
                     return rx.Observable
-                        .from(keys)
-                        .distinct()
-                        .flatMap<Node>(
-                            (key: string): rx.Observable<Node> => {
-                                return this._navigator.graphService.node$(key);
-                            });
+                        .merge<Node>([
+                            next$,
+                            prev$,
+                            forward$,
+                            backward$,
+                            left$,
+                            right$,
+                            pano$,
+                        ]);
                 })
             .subscribe();
     }
 
     protected _deactivate(): void {
         this._nodeSubscription.dispose();
+    }
+
+    private _cache$(node: Node, direction: EdgeDirection, depth: number): rx.Observable<Node> {
+        return rx.Observable
+            .just(node)
+            .expand(
+                (n: Node): rx.Observable<Node> => {
+                    for (let edge of n.edges) {
+                        if (edge.data.direction === direction) {
+                            return this._navigator.graphService.node$(edge.to);
+                        }
+                    }
+
+                    return rx.Observable.empty<Node>();
+                })
+            .skip(1)
+            .take(depth);
     }
 }
 
