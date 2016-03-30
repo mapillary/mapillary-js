a4d2290f8a033408cacb694189c8506b606b6d20
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-03-30 15:51:20 +0200

    Cache steps, sequence and pano nodes one level.
    Do not cache steps from pano nodes because there can
    be many edges for each direction.

--- src/component/CacheComponent.ts
+++ src/component/CacheComponent.ts
@@ -1,49 +1,77 @@
 /// <reference path="../../typings/browser.d.ts" />
 
-import * as _ from "underscore";
 import * as rx from "rx";
 
-import {IEdge, EdgeDirection} from "../Edge";
+import {EdgeDirection} from "../Edge";
 import {Node} from "../Graph";
 import {ComponentService, Component} from "../Component";
 import {Container, Navigator} from "../Viewer";
 
 export class CacheComponent extends Component {
     public static componentName: string = "cache";
-    private _disposable: rx.IDisposable;
+
+    private _prespectiveDirections: EdgeDirection[];
+    private _panoDirections: EdgeDirection[];
+
+    private _nodeSubscription: rx.IDisposable;
 
     constructor(name: string, container: Container, navigator: Navigator) {
         super(name, container, navigator);
+
+        this._prespectiveDirections = [
+            EdgeDirection.Prev,
+            EdgeDirection.Next,
+            EdgeDirection.StepLeft,
+            EdgeDirection.StepRight,
+            EdgeDirection.StepForward,
+            EdgeDirection.StepBackward,
+            EdgeDirection.Pano,
+        ];
+
+        this._panoDirections = [
+            EdgeDirection.Prev,
+            EdgeDirection.Next,
+            EdgeDirection.Pano,
+        ];
     }
 
     protected _activate(): void {
-        this._disposable = this._navigator.stateService.currentNode$.subscribe((node: Node) => {
-            _.map(node.edges, (edge: IEdge): void => {
-                if (edge.data.direction === EdgeDirection.Next) {
-                    this._navigator.graphService.node$(edge.to).first().subscribe((node2: Node) => {
-                        _.map(node2.edges, (edge2: IEdge): void => {
-                            if (edge2.data.direction === EdgeDirection.Next) {
-                                this._navigator.graphService.node$(edge2.to).first().subscribe();
-                            }
-                        });
-                    });
-                }
+        this._nodeSubscription = this._navigator.stateService.currentNode$
+            .map<string[]>(
+                (node: Node): string[] => {
+                    let keys: string[] = [];
 
-                if (edge.data.direction === EdgeDirection.Prev) {
-                    this._navigator.graphService.node$(edge.to).first().subscribe((node2: Node) => {
-                        _.map(node2.edges, (edge2: IEdge): void => {
-                            if (edge2.data.direction === EdgeDirection.Prev) {
-                                this._navigator.graphService.node$(edge2.to).first().subscribe();
+                    for (let edge of node.edges) {
+                        if (node.pano) {
+                            if (this._panoDirections.indexOf(edge.data.direction) === -1) {
+                                continue;
                             }
-                        });
-                    });
-                }
-            });
-        });
+                        } else {
+                            if (this._prespectiveDirections.indexOf(edge.data.direction) === -1) {
+                                continue;
+                            }
+                        }
+
+                        keys.push(edge.to);
+                    }
+
+                    return keys;
+                })
+            .flatMapLatest<Node>(
+                (keys: string[]): rx.Observable<Node> => {
+                    return rx.Observable
+                        .from(keys)
+                        .distinct()
+                        .flatMap<Node>(
+                            (key: string): rx.Observable<Node> => {
+                                return this._navigator.graphService.node$(key);
+                            });
+                })
+            .subscribe();
     }
 
     protected _deactivate(): void {
-        this._disposable.dispose();
+        this._nodeSubscription.dispose();
     }
 }
 
