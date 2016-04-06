fe671093b1c20805201b3d59a6549403447bd9dc
Pau Gargallo <pau.gargallo@gmail.com>
2016-04-06 13:17:11 +0200

    Render rects in GL

--- src/component/tag/TagGLRenderer.ts
+++ src/component/tag/TagGLRenderer.ts
@@ -30,6 +30,28 @@ export class TagGLRenderer {
 
     public updateTags(tags: ITag[]): void {
         this._needsRender = true;
+
+        for (let object of this._scene.children) {
+            this._scene.remove(object);
+            let line: THREE.Line = <THREE.Line>object;
+            line.geometry.dispose();
+            line.material.dispose();
+        }
+
+        for (let tag of tags) {
+            let lineGeometry: THREE.Geometry = new THREE.Geometry();
+            for (let i: number = 0; i < tag.polygon3d.length - 1; ++i) {
+                let a: number[] = tag.polygon3d[i];
+                let b: number[] = tag.polygon3d[i + 1];
+                lineGeometry.vertices.push(
+                    new THREE.Vector3(a[0], a[1], a[2]),
+                    new THREE.Vector3(b[0], b[1], b[2])
+                );
+            }
+            let lineMaterial: THREE.LineBasicMaterial = new THREE.LineBasicMaterial({ color: 0x00FF00, linewidth: 3 } );
+            let line: THREE.Line = new THREE.Line(lineGeometry, lineMaterial);
+            this._scene.add(line);
+        }
     }
 
     public dispose(): void {
