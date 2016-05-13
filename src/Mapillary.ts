a310ee4387d91844a5fd4a3e9c925e8b56d5aa57
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-05-13 11:39:52 +0200

    Expose tag component API in namespace.
    Define what classes should be exposed in component ts file.
    Import all properties as TagComponent and export in global
    Mapillary object.

--- src/Mapillary.ts
+++ src/Mapillary.ts
@@ -2,11 +2,10 @@
  * mapillary-js is a WebGL JavaScript library for exploring street level imagery
  * @name Mapillary
  */
-export {
-    OutlineTag,
-    RectGeometry,
-    TagComponent,
-} from "./Component";
+
 export {EdgeDirection} from "./Edge";
 export {RenderMode} from "./Render";
 export {ImageSize, Viewer} from "./Viewer";
+
+import * as TagComponent from "./component/tag/Tag";
+export {TagComponent};
---
+++ src/component/tag/Tag.ts
@@ -0,0 +1,4 @@
+export {GeometryTagError} from "./error/GeometryTagError";
+export {OutlineTag} from "./tag/OutlineTag";
+export {RectGeometry} from "./geometry/RectGeometry";
+export {TagComponent} from "./TagComponent";
