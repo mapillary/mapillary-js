df4a6719e1e45743318fffb44e177703fa6fa8ec
Nikola Plesa <nickplesha@gmail.com>
2016-07-26 11:50:23 +0200

    Fix moveDir example

--- src/viewer/Viewer.ts
+++ src/viewer/Viewer.ts
@@ -88,7 +88,7 @@ export class Viewer extends EventEmitter {
      *
      * @param {EdgeDirection} dir - Direction towards which to move
      * @example
-     * `viewer.moveToDir(Mapillary.EdgeDirection.NEXT);`
+     * `viewer.moveDir(Mapillary.EdgeDirection.Next);`
      */
     public moveDir(dir: EdgeDirection): when.Promise<Node> {
         return when.promise<Node>((resolve: any, reject: any): void => {
