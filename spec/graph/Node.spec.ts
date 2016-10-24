92195e28d6695c802d3be4e52e13e1ac24afd91f
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-10-24 11:32:36 +0200

    Unit test node and node cache.

--- spec/graph/Node.spec.ts
+++ spec/graph/Node.spec.ts
@@ -1,7 +1,316 @@
 /// <reference path="../../typings/index.d.ts" />
 
-import {IAPINavImIm, IAPINavImS} from "../../src/API";
-import {Node, Sequence} from "../../src/Graph";
+import {IAPINavImIm, IAPINavImS, ICoreNode, IFillNode} from "../../src/API";
+import {IMesh, NewNode, NewNodeCache, Node, Sequence} from "../../src/Graph";
+
+let createCoreNode: () => ICoreNode = (): ICoreNode => {
+    return {
+        cl: { lat: 0, lon: 0},
+        key: "key",
+        l: { lat: 0, lon: 0 },
+        sequence: { key: "skey" },
+    };
+};
+
+let createFillNode: () => IFillNode = (): IFillNode => {
+    return {
+        atomic_scale: 0,
+        c_rotation: [0, 0, 0],
+        ca: 0,
+        calt: 0,
+        captured_at: 0,
+        cca: 0,
+        cfocal: 0,
+        gpano: null,
+        height: 0,
+        merge_cc: 0,
+        merge_version: 0,
+        orientation: 0,
+        user: { key: "key", username: "username"},
+        width: 0,
+    };
+};
+
+describe("NewNode", () => {
+    it("should create a node", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+
+        expect(node).toBeDefined();
+    });
+});
+
+describe("NewNode.full", () => {
+    it("should make node full", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+
+        expect(node.full).toBe(false);
+
+        let fillNode: IFillNode = createFillNode();
+
+        node.makeFull(fillNode);
+
+        expect(node.full).toBe(true);
+    });
+
+    it("should throw when fill is null", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+
+        expect(() => { node.makeFull(null); }).toThrowError(Error);
+    });
+});
+
+describe("NewNode.dispose", () => {
+    it("should clear core and fill properties", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+        node.makeFull(fillNode);
+
+        node.dispose();
+
+        expect(node.full).toBe(false);
+
+        expect(() => { return node.key; }).toThrowError(Error);
+    });
+
+    it("should dipose cache", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let nodeCache: NewNodeCache = new NewNodeCache();
+
+        let disposeSpy: jasmine.Spy = spyOn(nodeCache, "dispose");
+        disposeSpy.and.stub();
+
+        node.initializeCache(nodeCache);
+
+        node.dispose();
+
+        expect(disposeSpy.calls.count()).toBe(1);
+    });
+});
+
+describe("NewNode.fullPano", () => {
+    it("should not be a full pano when gpano does not exist", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+
+        fillNode.gpano = null;
+
+        node.makeFull(fillNode);
+
+        expect(node.fullPano).toBe(false);
+    });
+
+    it("should not be full pano when cropped left", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+
+        fillNode.gpano = {
+            CroppedAreaImageHeightPixels: 1,
+            CroppedAreaImageWidthPixels: 1,
+            CroppedAreaLeftPixels: 0.5,
+            CroppedAreaTopPixels: 0,
+            FullPanoHeightPixels: 1,
+            FullPanoWidthPixels: 1,
+        };
+
+        node.makeFull(fillNode);
+
+        expect(node.fullPano).toBe(false);
+    });
+
+    it("should not be full pano when cropped top", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+
+        fillNode.gpano = {
+            CroppedAreaImageHeightPixels: 1,
+            CroppedAreaImageWidthPixels: 1,
+            CroppedAreaLeftPixels: 0,
+            CroppedAreaTopPixels: 0.5,
+            FullPanoHeightPixels: 1,
+            FullPanoWidthPixels: 1,
+        };
+
+        node.makeFull(fillNode);
+
+        expect(node.fullPano).toBe(false);
+    });
+
+    it("should not be full pano when cropped right", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+
+        fillNode.gpano = {
+            CroppedAreaImageHeightPixels: 1,
+            CroppedAreaImageWidthPixels: 0.5,
+            CroppedAreaLeftPixels: 0,
+            CroppedAreaTopPixels: 0,
+            FullPanoHeightPixels: 1,
+            FullPanoWidthPixels: 1,
+        };
+
+        node.makeFull(fillNode);
+
+        expect(node.fullPano).toBe(false);
+    });
+
+    it("should not be full pano when cropped bottom", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+
+        fillNode.gpano = {
+            CroppedAreaImageHeightPixels: 0.5,
+            CroppedAreaImageWidthPixels: 1,
+            CroppedAreaLeftPixels: 0,
+            CroppedAreaTopPixels: 0,
+            FullPanoHeightPixels: 1,
+            FullPanoWidthPixels: 1,
+        };
+
+        node.makeFull(fillNode);
+
+        expect(node.fullPano).toBe(false);
+    });
+
+    it("should be full pano", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+
+        fillNode.gpano = {
+            CroppedAreaImageHeightPixels: 1,
+            CroppedAreaImageWidthPixels: 1,
+            CroppedAreaLeftPixels: 0,
+            CroppedAreaTopPixels: 0,
+            FullPanoHeightPixels: 1,
+            FullPanoWidthPixels: 1,
+        };
+
+        node.makeFull(fillNode);
+
+        expect(node.fullPano).toBe(true);
+    });
+});
+
+describe("NewNode.pano", () => {
+    it("should not be a pano when gpano does not exist", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+
+        fillNode.gpano = null;
+
+        node.makeFull(fillNode);
+
+        expect(node.pano).toBe(false);
+    });
+
+    it("should be a pano", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+
+        fillNode.gpano = {
+            CroppedAreaImageHeightPixels: 0.5,
+            CroppedAreaImageWidthPixels: 0.5,
+            CroppedAreaLeftPixels: 0.25,
+            CroppedAreaTopPixels: 0.25,
+            FullPanoHeightPixels: 1,
+            FullPanoWidthPixels: 1,
+        };
+
+        node.makeFull(fillNode);
+
+        expect(node.pano).toBe(true);
+    });
+});
+
+describe("NewNode.merged", () => {
+    it("should not be merged when not full", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+
+        expect(node.merged).toBe(false);
+    });
+
+    it("should not be merged because merge version is zero", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+
+        fillNode.merge_version = 0;
+
+        node.makeFull(fillNode);
+
+        expect(node.merged).toBe(false);
+    });
+
+    it("should be merged because merge version present and larger than zero", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+
+        fillNode.merge_version = 7;
+
+        node.makeFull(fillNode);
+
+        expect(node.merged).toBe(true);
+    });
+});
+
+describe("NewNode.assetsCached", () => {
+    it("should not be cached when core", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+
+        expect(node.assetsCached).toBe(false);
+    });
+
+    class NewNodeCacheMock extends NewNodeCache {
+        protected _overridingImage: HTMLImageElement;
+        protected _overridingMesh: IMesh;
+
+        public get image(): HTMLImageElement {
+            return this._overridingImage;
+        }
+
+        public set image(value: HTMLImageElement) {
+            this._overridingImage = value;
+        }
+
+        public get mesh(): IMesh {
+            return this._overridingMesh;
+        }
+
+        public set mesh(value: IMesh) {
+            this._overridingMesh = value;
+        }
+    }
+
+    it("should be cached when assets set", () => {
+        let coreNode: ICoreNode = createCoreNode();
+        let node: NewNode = new NewNode(coreNode);
+        let fillNode: IFillNode = createFillNode();
+        node.makeFull(fillNode);
+
+        let nodeCache: NewNodeCacheMock = new NewNodeCacheMock();
+        nodeCache.image = new Image();
+        nodeCache.mesh = { faces: [], vertices: [] };
+
+        node.initializeCache(nodeCache);
+
+        expect(node.assetsCached).toBe(true);
+    });
+});
 
 describe("Node", () => {
     let sequence: Sequence;
--- spec/graph/NodeCache.spec.ts
+++ spec/graph/NodeCache.spec.ts
@@ -108,4 +108,65 @@ describe("NodeCache.spatialEdges$", () => {
 
         nodeCache.cacheSpatialEdges([spatialEdge]);
     });
+
+    describe("NodeCache.resetSpatialEdges", () => {
+        it("should reset the spatial edges", () => {
+            let nodeCache: NewNodeCache = new NewNodeCache();
+
+            let spatialEdge: IEdge = {
+                data: {
+                    direction: EdgeDirection.StepForward,
+                    worldMotionAzimuth: 0,
+                },
+                from: "key1",
+                to: "key2",
+            };
+
+            nodeCache.cacheSpatialEdges([spatialEdge]);
+
+            expect(nodeCache.spatialEdges.cached).toBe(true);
+            expect(nodeCache.spatialEdges.edges.length).toBe(1);
+            expect(nodeCache.spatialEdges.edges[0].from).toBe(spatialEdge.from);
+
+            nodeCache.resetSpatialEdges();
+
+            expect(nodeCache.spatialEdges.cached).toBe(false);
+            expect(nodeCache.spatialEdges.edges.length).toBe(0);
+        });
+    });
+
+    describe("NodeCache.dispose", () => {
+        it("should clear all properties", () => {
+            let nodeCache: NewNodeCache = new NewNodeCache();
+
+            let sequencEdge: IEdge = {
+                data: {
+                    direction: EdgeDirection.StepForward,
+                    worldMotionAzimuth: 0,
+                },
+                from: "key1",
+                to: "key2",
+            };
+
+            let spatialEdge: IEdge = {
+                data: {
+                    direction: EdgeDirection.StepForward,
+                    worldMotionAzimuth: 0,
+                },
+                from: "key1",
+                to: "key2",
+            };
+
+            nodeCache.cacheSequenceEdges([sequencEdge]);
+            nodeCache.cacheSpatialEdges([spatialEdge]);
+
+            nodeCache.dispose();
+
+            expect(nodeCache.sequenceEdges.cached).toBe(false);
+            expect(nodeCache.sequenceEdges.edges.length).toBe(0);
+
+            expect(nodeCache.spatialEdges.cached).toBe(false);
+            expect(nodeCache.spatialEdges.edges.length).toBe(0);
+        });
+    });
 });
