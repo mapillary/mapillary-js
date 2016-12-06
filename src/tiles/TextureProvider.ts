32ce213a0fef84810e117bc7b468de1dcdb61e09
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2016-12-06 18:08:36 +0100

    Keep track of rendered tile pyramid.
    Avoid rendering the same tile again when going back
    to a previous tile level.

--- src/tiles/TextureProvider.ts
+++ src/tiles/TextureProvider.ts
@@ -32,7 +32,8 @@ export class TextureProvider {
     private _tileSize: number;
     private _maxLevel: number;
     private _currentLevel: number;
-    private _renderedLevelTiles: { [key: string]: boolean };
+    private _renderedCurrentLevelTiles: { [key: string]: boolean };
+    private _renderedTiles: { [level: string]: number[][] };
     private _width: number;
 
     constructor (
@@ -48,7 +49,7 @@ export class TextureProvider {
         this._width = width;
         this._height = height;
         this._maxLevel = Math.ceil(Math.log(Math.max(height, width)) / Math.log(2) - 1);
-        this._currentLevel = this._maxLevel;
+        this._currentLevel = -1;
         this._tileSize = 512;
 
         this._updated$ = new Subject<boolean>();
@@ -61,7 +62,8 @@ export class TextureProvider {
 
         this._abortFunctions = [];
         this._tileSubscriptions = {};
-        this._renderedLevelTiles = {};
+        this._renderedCurrentLevelTiles = {};
+        this._renderedTiles = {};
 
         this._background = background;
         this._camera = null;
@@ -129,9 +131,17 @@ export class TextureProvider {
 
         let currentLevel: number = Math.ceil(Math.log(size) / Math.log(2) - 1);
         if (currentLevel !== this._currentLevel) {
-            this._currentLevel = currentLevel;
-            this._renderedLevelTiles = {};
             this.abort();
+
+            this._currentLevel = currentLevel;
+            if (!(this._currentLevel in this._renderedTiles)) {
+                this._renderedTiles[this._currentLevel] = [];
+            }
+
+            this._renderedCurrentLevelTiles = {};
+            for (let tile of this._renderedTiles[this._currentLevel]) {
+                this._renderedCurrentLevelTiles[this._tileKey(tile)] = true;
+            }
         }
 
         let topLeft: number[] = this._getTileCoords([this._roi.bbox.minX, this._roi.bbox.minY]);
@@ -177,6 +187,65 @@ export class TextureProvider {
         this._fetchTiles(tiles);
     }
 
+    private _fetchTile(tile: number[], x: number, y: number, w: number, h: number, scaledX: number, scaledY: number): void {
+        let getTile: [Observable<HTMLImageElement>, Function] =
+            this._imageTileLoader.getTile(this._key, x, y, w, h, scaledX, scaledY);
+
+        let tile$: Observable<HTMLImageElement> = getTile[0];
+        let abort: Function = getTile[1];
+
+        this._abortFunctions.push(abort);
+
+        let tileKey: string = this._tileKey(tile);
+
+        let subscription: Subscription = tile$
+            .subscribe(
+                (image: HTMLImageElement): void => {
+                    this._renderToTarget(x, y, w, h, image, true);
+
+                    this._removeFromDictionary(tileKey, this._tileSubscriptions);
+                    this._removeFromArray(abort, this._abortFunctions);
+
+                    this._setTileRendered(tile, this._currentLevel);
+
+                    this._updated$.next(true);
+                },
+                (error: Error): void => {
+                    this._removeFromDictionary(tileKey, this._tileSubscriptions);
+                    this._removeFromArray(abort, this._abortFunctions);
+
+                    console.error(error);
+                });
+
+        if (!subscription.closed) {
+            this._tileSubscriptions[tileKey] = subscription;
+        }
+    }
+
+    private _fetchTiles(tiles: number[][]): void {
+        let tileSize: number = this._tileSize * Math.pow(2, this._maxLevel - this._currentLevel);
+        let maxTileSize: number = Math.pow(2, this._maxLevel + 1);
+        let tileScale: number = maxTileSize >= tileSize ? 1 : maxTileSize / tileSize;
+
+        for (let tile of tiles) {
+            let tileKey: string = this._tileKey(tile);
+            if (tileKey in this._renderedCurrentLevelTiles ||
+                tileKey in this._tileSubscriptions) {
+                continue;
+            }
+
+            let tileX: number = tileSize * tile[0];
+            let tileY: number = tileSize * tile[1];
+            let tileWidth: number = tileX + tileSize > this._width ? this._width - tileX : tileSize;
+            let tileHeight: number = tileY + tileSize > this._height ? this._height - tileY : tileSize;
+            let size: number = Math.max(tileWidth, tileHeight);
+            let scaledX: number = Math.floor(tileScale * (tileWidth < this._tileSize ? tileWidth : tileWidth / size * this._tileSize));
+            let scaledY: number = Math.floor(tileScale * (tileHeight < this._tileSize ? tileHeight : tileHeight / size * this._tileSize));
+
+            this._fetchTile(tile, tileX, tileY, tileWidth, tileHeight, scaledX, scaledY);
+        }
+    }
+
     private _getTileCoords(point: number[]): number[] {
         let tileSize: number = this._tileSize * Math.pow(2, this._maxLevel - this._currentLevel);
 
@@ -218,63 +287,6 @@ export class TextureProvider {
         }
 
         return tiles;
-    }
-
-    private _fetchTile(tileKey: string, x: number, y: number, w: number, h: number, scaledX: number, scaledY: number): void {
-        let getTile: [Observable<HTMLImageElement>, Function] =
-            this._imageTileLoader.getTile(this._key, x, y, w, h, scaledX, scaledY);
-
-        let tile$: Observable<HTMLImageElement> = getTile[0];
-        let abort: Function = getTile[1];
-
-        this._abortFunctions.push(abort);
-
-        let subscription: Subscription = tile$
-            .subscribe(
-                (image: HTMLImageElement): void => {
-                    this._renderToTarget(x, y, w, h, image, true);
-
-                    this._removeFromDictionary(tileKey, this._tileSubscriptions);
-                    this._removeFromArray(abort, this._abortFunctions);
-
-                    this._renderedLevelTiles[tileKey] = true;
-
-                    this._updated$.next(true);
-                },
-                (error: Error): void => {
-                    this._removeFromDictionary(tileKey, this._tileSubscriptions);
-                    this._removeFromArray(abort, this._abortFunctions);
-
-                    console.error(error);
-                });
-
-        if (!subscription.closed) {
-            this._tileSubscriptions[tileKey] = subscription;
-        }
-    }
-
-    private _fetchTiles(tiles: number[][]): void {
-        let tileSize: number = this._tileSize * Math.pow(2, this._maxLevel - this._currentLevel);
-        let maxTileSize: number = Math.pow(2, this._maxLevel + 1);
-        let tileScale: number = maxTileSize >= tileSize ? 1 : maxTileSize / tileSize;
-
-        for (let tile of tiles) {
-            let tileKey: string = this._tileKey(tile);
-            if (tileKey in this._renderedLevelTiles ||
-                tileKey in this._tileSubscriptions) {
-                continue;
-            }
-
-            let tileX: number = tileSize * tile[0];
-            let tileY: number = tileSize * tile[1];
-            let tileWidth: number = tileX + tileSize > this._width ? this._width - tileX : tileSize;
-            let tileHeight: number = tileY + tileSize > this._height ? this._height - tileY : tileSize;
-            let size: number = Math.max(tileWidth, tileHeight);
-            let scaledX: number = Math.floor(tileScale * (tileWidth < this._tileSize ? tileWidth : tileWidth / size * this._tileSize));
-            let scaledY: number = Math.floor(tileScale * (tileHeight < this._tileSize ? tileHeight : tileHeight / size * this._tileSize));
-
-            this._fetchTile(tileKey, tileX, tileY, tileWidth, tileHeight, scaledX, scaledY);
-        }
     }
 
     private _removeFromArray<T>(item: T, array: T[]): void {
@@ -324,6 +336,56 @@ export class TextureProvider {
         if (revoke) {
             window.URL.revokeObjectURL(image.src);
         }
+    }
+
+    private _setTileRendered(tile: number[], level: number): void {
+        let otherLevels: number[] =
+            Object.keys(this._renderedTiles)
+                .map(
+                    (key: string): number => {
+                        return parseInt(key, 10);
+                    })
+                .filter(
+                    (renderedLevel: number): boolean => {
+                        return renderedLevel !== level;
+                    });
+
+        for (let otherLevel of otherLevels) {
+            let scale: number = Math.pow(2, otherLevel - level);
+
+            if (otherLevel < level) {
+                let x: number = Math.floor(scale * tile[0]);
+                let y: number = Math.floor(scale * tile[1]);
+
+                for (let otherTile of this._renderedTiles[otherLevel].slice()) {
+                    if (otherTile[0] === x && otherTile[1] === y) {
+                        let index: number = this._renderedTiles[otherLevel].indexOf(otherTile);
+                        this._renderedTiles[otherLevel].splice(index, 1);
+                    }
+                }
+            } else {
+                let startX: number = scale * tile[0];
+                let endX: number = startX + scale - 1;
+                let startY: number = scale * tile[1];
+                let endY: number = startY + scale - 1;
+
+                for (let otherTile of this._renderedTiles[otherLevel].slice()) {
+                    if (otherTile[0] >= startX && otherTile[0] <= endX &&
+                        otherTile[1] >= startY && otherTile[1] <= endY) {
+                        let index: number = this._renderedTiles[otherLevel].indexOf(otherTile);
+                        this._renderedTiles[otherLevel].splice(index, 1);
+
+                    }
+                }
+            }
+
+            if (this._renderedTiles[otherLevel].length === 0) {
+                delete this._renderedTiles[otherLevel];
+            }
+        }
+
+        this._renderedTiles[level].push(tile);
+        this._renderedCurrentLevelTiles[this._tileKey(tile)] = true;
     }
 
     private _tileKey(tile: number[]): string {
