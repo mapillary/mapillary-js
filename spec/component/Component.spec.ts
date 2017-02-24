a10832201fb27d646050c0600640b7330ba03d45
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2017-02-24 13:16:40 +0000

    Only emit configuration when it has changed.

---
+++ spec/component/Component.spec.ts
@@ -0,0 +1,86 @@
+import {
+    Component,
+    IComponentConfiguration,
+} from "../../src/Component";
+
+interface ITestConfiguration extends IComponentConfiguration {
+    test?: string;
+}
+
+class TestComponent extends Component<ITestConfiguration> {
+    protected _activate(): void { /*noop*/ }
+    protected _deactivate(): void { /*noop*/ }
+
+    protected _getDefaultConfiguration(): ITestConfiguration {
+        return {};
+    }
+}
+
+describe("Component.ctor", () => {
+    it("should be defined", () => {
+        let component: TestComponent = new TestComponent("test", undefined, undefined);
+
+        expect(component).toBeDefined();
+    });
+});
+
+describe("Component.configuration$", () => {
+    it("should be emtpy default configuration", () => {
+        let component: TestComponent = new TestComponent("test", undefined, undefined);
+
+        component.configuration$
+            .subscribe(
+                (configuration: IComponentConfiguration): void => {
+                    expect(Object.keys(configuration).length).toBe(0);
+                });
+    });
+
+    it("should emit when configured", (done: Function) => {
+        let component: TestComponent = new TestComponent("test", undefined, undefined);
+
+        component.configuration$
+            .skip(1)
+            .subscribe(
+                (configuration: ITestConfiguration): void => {
+                    expect(Object.keys(configuration).length).toBe(1);
+                    expect(configuration.test).toBe("test");
+                    done();
+                });
+
+        component.configure({ test: "test" });
+    });
+
+    it("should emit when configured value is changed", (done: Function) => {
+        let component: TestComponent = new TestComponent("test", undefined, undefined);
+
+        component.configuration$
+            .skip(2)
+            .subscribe(
+                (configuration: ITestConfiguration): void => {
+                    expect(Object.keys(configuration).length).toBe(1);
+                    expect(configuration.test).toBe("testchanged");
+                    done();
+                });
+
+        component.configure({ test: "test" });
+        component.configure({ test: "testchanged" });
+    });
+
+    it("should not emit when configured with same value", () => {
+        let component: TestComponent = new TestComponent("test", undefined, undefined);
+
+        let emitCount: number = 0;
+
+        component.configuration$
+            .skip(1)
+            .subscribe(
+                (configuration: ITestConfiguration): void => {
+                    emitCount++;
+                });
+
+        component.configure({ test: "test" });
+        component.configure({ test: "test" });
+
+        expect(emitCount).toBe(1);
+    });
+});
