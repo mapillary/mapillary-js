8e406fa19f992a458ed69fb75166bcf6acff578f
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2017-03-20 11:02:43 +0000

    Add API for intersecting objects in marker scene.

--- src/component/marker/MarkerScene.ts
+++ src/component/marker/MarkerScene.ts
@@ -8,11 +8,15 @@ import {Marker} from "../../Component";
 export class MarkerScene {
     private _needsRender: boolean;
     private _markers: { [key: string]: Marker };
+    private _objectMarkers: { [id: string]: string };
+    private _raycaster: THREE.Raycaster;
     private _scene: THREE.Scene;
 
     constructor() {
         this._needsRender = false;
         this._markers = {};
+        this._objectMarkers = {};
+        this._raycaster = new THREE.Raycaster();
         this._scene = new THREE.Scene();
     }
 
@@ -32,6 +36,9 @@ export class MarkerScene {
         marker.createGeometry(position);
         this._scene.add(marker.geometry);
         this._markers[marker.id] = marker;
+        for (let draggableId of marker.getDraggableObjectIds()) {
+            this._objectMarkers[draggableId] = marker.id;
+        }
 
         this._needsRender = true;
     }
@@ -48,6 +55,10 @@ export class MarkerScene {
         this._needsRender = true;
     }
 
+    public get(id: string): Marker {
+        return this._markers[id];
+    }
+
     public getAll(): Marker[] {
         return Object
             .keys(this._markers)
@@ -56,6 +67,19 @@ export class MarkerScene {
 
     public has(id: string): boolean {
         return id in this._markers;
+    }
+
+    public intersectObjects([viewportX, viewportY]: number[], camera: THREE.Camera): string {
+        this._raycaster.setFromCamera(new THREE.Vector2(viewportX, viewportY), camera);
+
+        const intersects: THREE.Intersection[] = this._raycaster.intersectObjects(this._scene.children, true);
+        for (const intersect of intersects) {
+            if (intersect.object.uuid in this._objectMarkers) {
+                return this._objectMarkers[intersect.object.uuid];
+            }
+        }
+
+        return null;
     }
 
     public lerpAltitude(id: string, alt: number, alpha: number): void {
@@ -93,7 +117,7 @@ export class MarkerScene {
         }
 
         const marker: Marker = this._markers[id];
-        marker.updatePosition(position);
+        marker.updatePosition(position, latLon);
 
         this._needsRender = true;
     }
@@ -101,6 +125,10 @@ export class MarkerScene {
     private _dispose(id: string): void {
         const marker: Marker = this._markers[id];
         this._scene.remove(marker.geometry);
+        for (let draggableId of marker.getDraggableObjectIds()) {
+            delete this._objectMarkers[draggableId];
+        }
+
         marker.disposeGeometry();
 
         delete this._markers[id];
