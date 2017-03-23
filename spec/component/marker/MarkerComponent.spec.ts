b0a18810638138a84f20451b219756c1d5941ecd
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2017-03-23 10:17:25 +0000

    Test marker component deactivation.

--- spec/component/marker/MarkerComponent.spec.ts
+++ spec/component/marker/MarkerComponent.spec.ts
@@ -12,7 +12,19 @@ import {Container} from "../../../src/Viewer";
 import {ContainerMockCreator} from "../../helper/ContainerMockCreator.spec";
 import {NavigatorMockCreator} from "../../helper/NavigatorMockCreator.spec";
 
-describe("MarkerComponent", () => {
+describe("MarkerComponent.ctor", () => {
+    it("should be defined", () => {
+        const markerComponent: MarkerComponent =
+            new MarkerComponent(
+                MarkerComponent.componentName,
+                new ContainerMockCreator().createMock(),
+                new NavigatorMockCreator().createMock());
+
+        expect(markerComponent).toBeDefined();
+    });
+});
+
+describe("MarkerComponent.add", () => {
     let markerComponent: MarkerComponent;
 
     beforeEach(() => {
@@ -28,7 +40,7 @@ describe("MarkerComponent", () => {
         markerComponent.activate();
     });
 
-    it("It should be able to create two markers at the exact same position", () => {
+    it("should be able to create two markers at the exact same position", () => {
         let m1: Marker = new SimpleMarker("1", { lat: 0, lon: 0 }, {});
         markerComponent.add([m1]);
         expect(markerComponent.getAll().length).toBe(1);
@@ -44,7 +56,7 @@ describe("MarkerComponent", () => {
         expect(markerComponent.getAll().length).toBe(0);
     });
 
-    it("It should be able to update an marker by using the same id", () => {
+    it("should be able to update an marker by using the same id", () => {
         let m1: Marker = new SimpleMarker("1", { lat: 0, lon: 0 }, {});
         markerComponent.add([m1]);
         expect(markerComponent.getAll().length).toBe(1);
@@ -56,5 +68,21 @@ describe("MarkerComponent", () => {
         expect(markerComponent.getAll().length).toBe(1);
         expect(markerComponent.get("1").latLon.lat).toBe(1);
         expect(markerComponent.get("1").latLon.lon).toBe(1);
+    });
+});
+
+describe("MarkerComponent.deactivate", () => {
+    it("should deactivate properly", () => {
+        const containerMock: Container = new ContainerMockCreator().createMock();
+        (<jasmine.Spy>containerMock.mouseService.filtered$).and.returnValue(Observable.empty<MouseEvent>());
+
+        const markerComponent: MarkerComponent =
+            new MarkerComponent(
+                MarkerComponent.componentName,
+                containerMock,
+                new NavigatorMockCreator().createMock());
+
+        markerComponent.activate();
+        markerComponent.deactivate();
     });
 });
