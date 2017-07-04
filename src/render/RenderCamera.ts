b5341a989c1ac9316356520d44d685cf0132a963
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2017-07-04 08:51:51 +0000

    build: update three.js
    Do not calculate matrix world inverse becasue it is
    done automatically by three js for cameras when
    updating matrix world from v0.86.

--- src/render/RenderCamera.ts
+++ src/render/RenderCamera.ts
@@ -129,7 +129,6 @@ export class RenderCamera {
 
         this._perspective.updateMatrix();
         this._perspective.updateMatrixWorld(false);
-        this._perspective.matrixWorldInverse.getInverse(this._perspective.matrixWorld);
 
         this._changed = true;
     }
