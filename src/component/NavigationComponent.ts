8c13560c490e10c3890d6b26fa2edd0f61b736b3
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2017-08-23 09:05:25 +0000

    feat: navigation component without css transforms
    Fixes #177
    
    Render navigation component arrows for sequence as well as
    spatial.
    Position arrows according to direction and sequence component.
    Use base64 encoded svgs rotated with svg transform to avoid
    more svgs in dist folder.

--- src/component/NavigationComponent.ts
+++ src/component/NavigationComponent.ts
@@ -20,49 +20,81 @@ export class NavigationComponent extends Component<IComponentConfiguration> {
 
     private _renderSubscription: Subscription;
 
-    private _dirNames: {[dir: number]: string};
+    private _seqNames: { [dir: string]: string };
+    private _spaTopNames: { [dir: string]: string };
+    private _spaMiddleNames: { [dir: string]: string };
+    private _spaBottomNames: { [dir: string]: string };
 
     constructor(name: string, container: Container, navigator: Navigator) {
         super(name, container, navigator);
 
-        this._dirNames = {};
-        this._dirNames[EdgeDirection.StepForward] = "Forward";
-        this._dirNames[EdgeDirection.StepBackward] = "Backward";
-        this._dirNames[EdgeDirection.StepLeft] = "Left";
-        this._dirNames[EdgeDirection.StepRight] = "Right";
-        this._dirNames[EdgeDirection.TurnLeft] = "Turnleft";
-        this._dirNames[EdgeDirection.TurnRight] = "Turnright";
-        this._dirNames[EdgeDirection.TurnU] = "Turnaround";
+        this._seqNames = {};
+        this._seqNames[EdgeDirection[EdgeDirection.Prev]] = "Prev";
+        this._seqNames[EdgeDirection[EdgeDirection.Next]] = "Next";
+
+        this._spaTopNames = {};
+        this._spaTopNames[EdgeDirection[EdgeDirection.TurnLeft]] = "Turnleft";
+        this._spaTopNames[EdgeDirection[EdgeDirection.StepForward]] = "Forward";
+        this._spaTopNames[EdgeDirection[EdgeDirection.TurnRight]] = "Turnright";
+
+        this._spaMiddleNames = {};
+        this._spaMiddleNames[EdgeDirection[EdgeDirection.StepLeft]] = "Left";
+        this._spaMiddleNames[EdgeDirection[EdgeDirection.StepRight]] = "Right";
+
+        this._spaBottomNames = {};
+        this._spaBottomNames[EdgeDirection[EdgeDirection.TurnU]] = "Turnaround";
+        this._spaBottomNames[EdgeDirection[EdgeDirection.StepBackward]] = "Backward";
     }
 
     protected _activate(): void {
         this._renderSubscription = this._navigator.stateService.currentNode$
             .switchMap(
-                (node: Node): Observable<IEdge[]> => {
-                    return node.pano ?
-                        Observable.of<IEdge[]>([]) :
-                        Observable.combineLatest<IEdge[]>(
-                            node.sequenceEdges$,
-                            node.spatialEdges$,
-                            (seq: IEdgeStatus, spa: IEdgeStatus): IEdge[] => {
-                                return seq.edges.concat(spa.edges);
+                (node: Node): Observable<EdgeDirection[]> => {
+                    const sequenceEdges$: Observable<EdgeDirection[]> = node.sequenceEdges$
+                        .map(
+                            (status: IEdgeStatus): EdgeDirection[] => {
+                                return status.edges
+                                    .map(
+                                        (edge: IEdge): EdgeDirection => {
+                                            return edge.data.direction;
+                                        });
+                            });
+
+                    const spatialEdges$: Observable<EdgeDirection[]> = node.pano ?
+                        Observable.of<EdgeDirection[]>([]) :
+                        node.spatialEdges$
+                            .map(
+                                (status: IEdgeStatus): EdgeDirection[] => {
+                                    return status.edges
+                                        .map(
+                                            (edge: IEdge): EdgeDirection => {
+                                                return edge.data.direction;
+                                            });
+                                });
+
+                    return Observable
+                        .combineLatest(
+                            sequenceEdges$,
+                            spatialEdges$)
+                        .map(
+                            ([seq, spa]: [EdgeDirection[], EdgeDirection[]]): EdgeDirection[] => {
+                                 return seq.concat(spa);
                             });
                 })
             .map(
-                (edges: IEdge[]): IVNodeHash => {
-                    let btns: vd.VNode[] = [];
+                (edgeDirections: EdgeDirection[]): IVNodeHash => {
+                    const seqs: vd.VNode[] = this._createArrowRow(this._seqNames, edgeDirections);
+                    const spaTops: vd.VNode[] = this._createArrowRow(this._spaTopNames, edgeDirections);
+                    const spaMiddles: vd.VNode[] = this._createArrowRow(this._spaMiddleNames, edgeDirections);
+                    const spaBottoms: vd.VNode[] = this._createArrowRow(this._spaBottomNames, edgeDirections);
 
-                    for (let edge of edges) {
-                        let direction: EdgeDirection = edge.data.direction;
-                        let name: string = this._dirNames[direction];
-                        if (name == null) {
-                            continue;
-                        }
+                    const seqContainer: vd.VNode = vd.h(`div.NavigationSequence`, seqs);
+                    const spaTopContainer: vd.VNode = vd.h(`div.NavigationSpatialTop`, spaTops);
+                    const spaMiddleContainer: vd.VNode = vd.h(`div.NavigationSpatialMiddle`, spaMiddles);
+                    const spaBottomContainer: vd.VNode = vd.h(`div.NavigationSpatialBottom`, spaBottoms);
+                    const spaContainer: vd.VNode = vd.h(`div.NavigationSpatial`, [spaTopContainer, spaMiddleContainer, spaBottomContainer]);
 
-                        btns.push(this._createVNode(direction, name));
-                    }
-
-                    return {name: this._name, vnode: vd.h(`div.NavigationComponent`, btns)};
+                    return { name: this._name, vnode: vd.h(`div.NavigationContainer`, [seqContainer, spaContainer]) };
                 })
             .subscribe(this._container.domRenderer.render$);
     }
@@ -75,7 +107,26 @@ export class NavigationComponent extends Component<IComponentConfiguration> {
         return {};
     }
 
-    private _createVNode(direction: EdgeDirection, name: string): vd.VNode {
+    private _createArrowRow(arrowNames: { [dir: string]: string }, edgeDirections: EdgeDirection[]): vd.VNode[] {
+        const arrows: vd.VNode[] = [];
+
+        for (const arrowName in arrowNames) {
+            if (!(arrowNames.hasOwnProperty(arrowName))) {
+                continue;
+            }
+
+            const direction: EdgeDirection = EdgeDirection[<keyof typeof EdgeDirection>arrowName];
+            if (edgeDirections.indexOf(direction) !== -1) {
+                arrows.push(this._createVNode(direction, arrowNames[arrowName], "visible"));
+            } else {
+                arrows.push(this._createVNode(direction, arrowNames[arrowName], "hidden"));
+            }
+        }
+
+        return arrows;
+    }
+
+    private _createVNode(direction: EdgeDirection, name: string, visibility: string): vd.VNode {
         return vd.h(
             `span.Direction.Direction${name}`,
             {
@@ -84,6 +135,9 @@ export class NavigationComponent extends Component<IComponentConfiguration> {
                         .subscribe(
                             (node: Node): void => { return; },
                             (error: Error): void => { console.error(error); });
+                },
+                style: {
+                    visibility: visibility,
                 },
             },
             []);
