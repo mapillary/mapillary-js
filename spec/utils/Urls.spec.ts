8dec67c8f7e56ca4eb09fc5af9b7c23f85f368e6
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2018-02-13 08:29:49 +0000

    test: set urls correctly

---
+++ spec/utils/Urls.spec.ts
@@ -0,0 +1,26 @@
+/// <reference path="../../typings/index.d.ts" />
+
+import {Urls} from "../../src/Utils";
+import {IUrlOptions} from "../../src/Viewer";
+
+describe("Urls.setOptions", () => {
+    it("should set all option properties", () => {
+        const options: IUrlOptions = {
+            apiHost: "test-api",
+            exploreHost: "test-explore",
+            imageHost: "test-image",
+            imageTileHost: "test-imageTile",
+            meshHost: "test-mesh",
+            scheme: "test-scheme",
+        };
+
+        Urls.setOptions(options);
+
+        expect(Urls.falcorModel("client-id")).toContain(options.apiHost);
+        expect(Urls.explore).toContain(options.exploreHost);
+        expect(Urls.thumbnail("key", 640)).toContain(options.imageHost);
+        expect(Urls.tileDomain).toContain(options.imageTileHost);
+        expect(Urls.protoMesh("key")).toContain(options.meshHost);
+        expect(Urls.tileScheme).toContain(options.scheme);
+    });
+});
