fc4b5666a13bfef522f7b95a7632e8100a196922
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2018-05-24 12:26:56 +0000

    feat: restyle attribution
    Add date and logo.

--- src/component/AttributionComponent.ts
+++ src/component/AttributionComponent.ts
@@ -29,7 +29,7 @@ export class AttributionComponent extends Component<IComponentConfiguration> {
         this._disposable = this._navigator.stateService.currentNode$
             .map(
                 (node: Node): IVNodeHash => {
-                    return {name: this._name, vnode: this._getAttributionNode(node.username, node.key)};
+                    return {name: this._name, vnode: this._getAttributionNode(node.username, node.key, node.capturedAt)};
                 })
             .subscribe(this._container.domRenderer.render$);
     }
@@ -42,20 +42,25 @@ export class AttributionComponent extends Component<IComponentConfiguration> {
         return {};
     }
 
-    private _getAttributionNode(username: string, key: string): vd.VNode {
-        return vd.h("div.Attribution", {}, [
-            vd.h("a", {href: Urls.exporeUser(username),
-                       target: "_blank",
-                       textContent: `@${username}`,
-                      },
-                 []),
-            vd.h("span", {textContent: "|"}, []),
-            vd.h("a", {href: Urls.exporeImage(key),
-                       target: "_blank",
-                       textContent: "mapillary.com",
-                      },
-                 []),
-        ]);
+    private _getAttributionNode(username: string, key: string, capturedAt: number): vd.VNode {
+        const mapillaryIcon: vd.VNode = vd.h("div.AttributionMapillaryIcon", []);
+        const mapillaryLink: vd.VNode = vd.h("a.AttributionIconContainer", { href: Urls.explore, target: "_blank" }, [mapillaryIcon]);
+
+        const imageByContent: vd.VNode = vd.h("div.AttributionUsername", { textContent: `image by ${username}` }, []);
+
+        const date: string[] = new Date(capturedAt).toDateString().split(" ");
+        const formatted: string = date.length > 3 ?
+            [date[1], date[2] + ",", date[3]].join(" ") :
+            date.join(" ");
+        const dateContent: vd.VNode = vd.h("div.AttributionDate", { textContent: formatted }, []);
+
+        const imageLink: vd.VNode =
+            vd.h(
+                "a.AttributionImageContainer",
+                { href: Urls.exporeImage(key), target: "_blank" },
+                [imageByContent, dateContent]);
+
+        return vd.h("div.AttributionContainer", {}, [mapillaryLink, imageLink]);
     }
 }
 
