2de4f881ef208dd092912a31ad0d77eea758b43c
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2018-06-11 11:50:17 +0000

    chore: update typescript to 2.9

--- src/graph/ImageLoadingService.ts
+++ src/graph/ImageLoadingService.ts
@@ -3,14 +3,16 @@ import {Observable} from "rxjs/Observable";
 
 import {ILoadStatus, Node} from "../Graph";
 
+type LoadStatus = [{ [key: string]: ILoadStatus }, boolean];
+
 export class ImageLoadingService {
     private _loadnode$: Subject<Node> = new Subject<Node>();
     private _loadstatus$: Observable<{[key: string]: ILoadStatus}>;
 
     constructor () {
         this._loadstatus$ = this._loadnode$
-            .scan(
-                ([nodes]: [{[key: string]: ILoadStatus}, boolean], node: Node): [{[key: string]: ILoadStatus}, boolean] => {
+            .scan<Node, LoadStatus>(
+                ([nodes]: LoadStatus, node: Node): [{[key: string]: ILoadStatus}, boolean] => {
                     let changed: boolean = false;
                     if (node.loadStatus.total === 0 || node.loadStatus.loaded === node.loadStatus.total) {
                         if (node.key in nodes) {
