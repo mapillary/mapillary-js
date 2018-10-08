14ed6b753759b14ecf5fd536ced433a458468ed3
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2018-10-08 09:54:44 +0000

    chore: update dependencies
    Keep Three.js att 0.95 because of incorrect movement
    after upgrade to 0.96.

--- spec/helper/DOMRendererMockCreator.spec.ts
+++ spec/helper/DOMRendererMockCreator.spec.ts
@@ -11,6 +11,7 @@ export class DOMRendererMockCreator extends MockCreatorBase<DOMRenderer> {
     public create(): DOMRenderer {
         const mock: DOMRenderer = new MockCreator().create(DOMRenderer, "DOMRenderer");
 
+        this._mockProperty(mock, "element$", new Subject<IVNodeHash>());
         this._mockProperty(mock, "render$", new Subject<IVNodeHash>());
 
         return mock;
