8db0e7a248d3f23304b2eb2b973c8f673cb14246
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2018-10-31 10:03:08 +0000

    test: keyboard service key up observable

--- spec/viewer/KeyboardService.spec.ts
+++ spec/viewer/KeyboardService.spec.ts
@@ -32,3 +32,24 @@ describe("KeyboardService.keyDown$", () => {
         canvasContainer.dispatchEvent(keyboardEvent);
     });
 });
+
+describe("KeyboardService.keyUp$", () => {
+    it("should emit", (done: Function) => {
+        const canvasContainer: HTMLElement = document.createElement("div");
+
+        const keyboardService: KeyboardService = new KeyboardService(canvasContainer);
+
+        keyboardService.keyUp$
+            .subscribe(
+                (event: KeyboardEvent): void => {
+                    expect(event).toBeDefined();
+                    expect(event.type).toBe("keyup");
+                    expect(event.key).toBe("+");
+
+                    done();
+                });
+
+        const keyboardEvent: KeyboardEvent = EventHelper.createKeyboardEvent("keyup", { key: "+" });
+        canvasContainer.dispatchEvent(keyboardEvent);
+    });
+});
