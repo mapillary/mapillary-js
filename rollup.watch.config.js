abde985e5f11e5a358b09c2d297591d8f1787835
Oscar Lorentzon <oscarlorentzon@users.noreply.github.com>
2021-03-09 14:30:57 +0100

    chore: tsc and rollup watch build

---
+++ rollup.watch.config.js
@@ -0,0 +1,34 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import virtual from '@rollup/plugin-virtual';
+
+const virtualModules = {
+    './getXMLHttpRequest': `
+    export default function getXMLHttpRequest() {
+        return new XmlHTTPRequest();
+    }`,
+    'domain': 'export default undefined;',
+};
+
+const resolveOptions = { preferBuiltins: false };
+
+export default [
+    {
+        input: 'build/esm/src/Mapillary.js',
+        output: [
+            {
+                file: 'dist/mapillary.js',
+                format: 'umd',
+                name: 'Mapillary',
+                sourcemap: true,
+            }
+        ],
+        plugins: [
+            sourcemaps(),
+            virtual(virtualModules),
+            resolve(resolveOptions),
+            commonjs(),
+        ],
+    },
+];
