732cf5909cd2e8720d1526c047926cf5a1c67de2
Oscar Lorentzon <oscarlorentzon@users.noreply.github.com>
2021-03-09 17:06:52 +0100

    chore: incremental watch build
    Remove unused dependencies.

--- server.js
+++ server.js
@@ -1,83 +1,24 @@
 'use strict';
 
-const autoprefixer = require('autoprefixer');
-const browserify = require('browserify-middleware');
 const express = require('express');
-const fs = require('fs')
 const path = require('path');
-const postcss = require('postcss');
-const tsify = require('tsify');
-const inline = require('postcss-inline-svg')({
-    paths: ['./styles'],
-    encode: svg => Buffer.from(svg).toString('base64'),
-    transform: encoded => `"data:image/svg+xml;base64,${encoded}"`
-});
-const cssnano = require('cssnano')({
-    preset: ['default', {
-        normalizeWhitespace: false,
-        svgo: {
-            plugins: [{
-                removeViewBox: false
-            }, {
-                removeDimensions: false
-            }],
-        },
-    }],
-});
+
+const PORT = 8000;
+
+const logger = (req, _, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
+    next();
+};
 
 const app = express();
-
-app.get('/dist/mapillary.js', browserify('./src/Mapillary.ts', {
-    cache: 'dynamic',
-    debug: true,
-    plugins: [{
-        plugin: 'tsify',
-        options: JSON.parse(fs.readFileSync('./tsconfig.json', 'utf8')).compilerOptions,
-    }],
-    precompile: true,
-    standalone: 'Mapillary',
-}));
-
-app.get('/dist/mapillary.min.css', (_, res) => {
-    const styles = path.join(__dirname, 'styles');
-    const css = fs.readdirSync(styles)
-        .map(filename => path.join(styles, filename))
-        .filter(
-            filepath => {
-                const isFile = fs.statSync(filepath).isFile();
-                if (!isFile) { return false; }
-                const isCss = path.extname(filepath) === '.css';
-                if (!isCss) { return false; }
-                return true;
-            })
-        .map(
-            filepath => {
-                return fs.readFileSync(filepath, { encoding: 'utf8' });
-            })
-        .reduce((acc, curr) => acc + curr, '');
-
-    const from = path.join(__dirname, 'styles', 'mapillary-js.css');
-    const map = false;
-    const processor = postcss([
-        autoprefixer,
-        inline,
-        cssnano,
-    ]);
-    processor.process(css, { from, map }).then(result => {
-        res.writeHead(200, {
-            'Content-Type': 'text/css'
-        });
-        res.end(result.css);
-    });
-});
-
-app.get('/debug', (_, res) => { res.redirect('/'); });
-app.use(express.static(path.join(__dirname, 'debug')));
+app.use(logger);
+app.use('/', express.static(path.join(__dirname, 'debug')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.get('/debug', (_, res) => { res.redirect('/'); });
 
-const port = 8000;
-app.listen(port, () => {
+app.listen(PORT, () => {
     const message =
-        `mapillary-js debug server running at http://localhost:${port}`;
+        `mapillary-js debug server running at ` +
+        `http://localhost:${PORT}`;
     console.log(message);
 });
