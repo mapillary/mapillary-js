1499c8c8b2828bda8cf099361207a38cc175a194
Oscar Lorentzon <oscarlorentzon@users.noreply.github.com>
2021-04-28 13:41:24 +0200

    feat: use octree for logaritmic raycast complexity

--- src/component/spatial/scene/SpatialIntersection.ts
+++ src/component/spatial/scene/SpatialIntersection.ts
@@ -4,11 +4,13 @@ import {
     Raycaster,
     Vector2,
 } from "three";
+import { SpatialOctree } from "./SpatialOctree";
 
 export class SpatialIntersection {
     private readonly _interactiveLayer: number;
     private readonly _objects: Object3D[];
     private readonly _objectImageMap: Map<string, string>;
+    private readonly _octree: SpatialOctree;
     private readonly _raycaster: Raycaster;
 
     private readonly _lineThreshold: number;
@@ -17,6 +19,7 @@ export class SpatialIntersection {
     constructor(raycaster?: Raycaster) {
         this._objects = [];
         this._objectImageMap = new Map();
+        this._octree = new SpatialOctree();
         this._raycaster = !!raycaster ? raycaster : new Raycaster();
 
         this._interactiveLayer = 1;
@@ -26,7 +29,7 @@ export class SpatialIntersection {
                 undefined,
                 undefined,
                 1,
-                3000);
+                10000);
 
         this._lineThreshold = 0.1;
         this._largeLineThreshold = 0.4;
@@ -41,22 +44,25 @@ export class SpatialIntersection {
     public add(
         object: Object3D,
         imageId: string): void {
-        const uuid = object.uuid
+        const uuid = object.uuid;
         this._objectImageMap.set(uuid, imageId);
         this._objects.push(object);
+        this._octree.add(object);
     }
 
     public intersectObjects(
         viewport: number[],
-        camera: Camera): string {
+        camera: Camera)
+        : string {
+
         this._raycaster.setFromCamera(
             new Vector2().fromArray(viewport),
             camera);
+        const objects = this._octree.intersect(this.raycaster.ray);
+        const intersects =
+            this._raycaster.intersectObjects(objects);
 
         const onMap = this._objectImageMap;
-        const intersects =
-            this._raycaster.intersectObjects(this._objects);
-
         for (const intersect of intersects) {
             const uuid = intersect.object.uuid;
             if (!onMap.has(uuid)) { continue; }
@@ -73,6 +79,7 @@ export class SpatialIntersection {
             for (const d of deleted) {
                 this._objectImageMap.delete(d.uuid);
             }
+            this._octree.remove(object)
         } else {
             console.warn(`Object does not exist`);
         }
