b49229a77a996cd8b3640bcac56a80bab7a57d5c
Oscar Lorentzon <oscarlorentzon@users.noreply.github.com>
2021-04-30 12:47:22 +0200

    fix: set initial camera controls correctly

---
+++ test/viewer/Modes.test.ts
@@ -0,0 +1,16 @@
+import { State } from "../../src/state/State";
+import { cameraControlsToState } from "../../src/viewer/Modes";
+import { CameraControls } from "../../src/viewer/enums/CameraControls";
+
+describe("cameraControlsToState", () => {
+    it("converts to valid states", () => {
+        expect(cameraControlsToState(CameraControls.Custom))
+            .toBe(State.Custom);
+        expect(cameraControlsToState(CameraControls.Earth))
+            .toBe(State.Earth);
+        expect(cameraControlsToState(CameraControls.Street))
+            .toBe(State.Traversing);
+        expect(cameraControlsToState(-1))
+            .toBe(null);
+    });
+});
