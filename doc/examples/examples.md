d73f9cf2769e0efc49c1fc5cc9c82e0077f12a0f
Oscar Lorentzon <oscarlorentzon@users.noreply.github.com>
2021-05-08 12:38:35 +0200

    docs: init docusaurus website

---
+++ doc/examples/examples.md
@@ -0,0 +1,5 @@
+---
+id: examples
+title: Examples
+slug: /
+---
---
+++ doc/src/pages/index.js
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: 'Visualization Platform',
+    imageUrl: 'img/mjs.png',
+    description: (
+      <>
+        MapillaryJS is a street imagery and semantic mapping visualization platform on the web. You can integrate MapillaryJS into your website with just a few lines of code.
+      </>
+    ),
+  },
+  {
+    title: 'Spatial Interactivity',
+    imageUrl: 'img/semantic.png',
+    description: (
+      <>
+        MapillaryJS takes spatial, sematic, and texture data and renders it using JavaScript and WebGL. You can interact with the undistorted virtual 3D world and navigate the imagery from the street level or a bird&apos;s eye view.
+      </>
+    ),
+  },
+  {
+    title: 'Extend and Augment',
+    imageUrl: 'img/dropoff.png',
+    description: (
+      <>
+        Use the MapillaryJS Extension API to extend, customize, and augment the MapillaryJS experience with your own data providers, semantic meshes, 3D models, camera controls, and interactivity.
+      </>
+    ),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/')}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map(({title, imageUrl, description}) => (
+                  <Feature
+                    key={title}
+                    title={title}
+                    imageUrl={imageUrl}
+                    description={description}
+                  />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
