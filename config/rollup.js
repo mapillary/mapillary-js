d1ac7feb0bf1774a65e20ec5a7c7c10fdaecf991
Oscar Lorentzon <oscarlorentzon@users.noreply.github.com>
2021-07-30 14:13:48 +0200

    chore: remove unused script + dev dependency

--- config/rollup.js
+++ config/rollup.js
@@ -1,40 +1,27 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import sourcemaps from 'rollup-plugin-sourcemaps';
-import virtual from '@rollup/plugin-virtual';
-
-const virtualModules = {
-    './getXMLHttpRequest': `
-    export default function getXMLHttpRequest() {
-        return new XmlHTTPRequest();
-    }`,
-    'domain': 'export default undefined;',
-};
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import sourcemaps from "rollup-plugin-sourcemaps";
 
 const resolveOptions = { preferBuiltins: false };
 
-export const plugins = [
-    sourcemaps(),
-    virtual(virtualModules),
-    resolve(resolveOptions),
-    commonjs(),
-];
+export const plugins = [sourcemaps(), resolve(resolveOptions), commonjs()];
 
 export const umdOutput = {
-    format: 'umd',
-    name: 'mapillary',
-    sourcemap: true,
-}
+  format: "umd",
+  name: "mapillary",
+  sourcemap: true,
+};
 
-export const srcInput = 'build/esm/src/mapillary.js';
+export const srcInput = "build/esm/src/mapillary.js";
 
 export const esm = {
-    input: srcInput,
-    output: [
-        {
-            file: 'dist/mapillary.module.js',
-            format: 'es',
-            sourcemap: true,
-        }],
-    plugins,
+  input: srcInput,
+  output: [
+    {
+      file: "dist/mapillary.module.js",
+      format: "es",
+      sourcemap: true,
+    },
+  ],
+  plugins,
 };
